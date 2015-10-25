c8c80e7b506a4f29bb897bc29d7e127d05983e81
CaptainS5 <xw1106@nyu.edu>
2015-10-24 21:58:15 -0400

    adding hard/soft and TargetCross to parsexxx.m and getStats.m

--- getStats.m
+++ getStats.m
@@ -3,11 +3,23 @@
 % obs_name is the same as in parseExpData.m, such as 'xiuyun';
 % doNeq when set to 0, Neq is not computed(such as when there are no noise_contrst=0 runs);
 %       when set to 1, Neq is computed
+%
 % doEfficiency is the same as doNeq;
 % the efficiency here is the 'high noise efficiency', which is E_ideal/(E-E0)
 % that means when noist contrast=0, there is no high-noise efficiency computed
+%
+% To be able to compute E0(needed for Neq and Efficiency), you should have noise contrast=0, 
+% whose conditon has the same letter size,eccentricity,and TargetCross with
+% other noise contrast~=0 conditions;
+%
+% To be able to compute Efficiency, besides E0, you should also have ideal
+% data with the same letter size, noisecontrast(~=0), noisedecayradius, eccentricity and noiseRadiusDeg
 
 function tab = getStats(newpath, obs_name, doNeq, doEfficiency)
+% newpath = '/Users/xiuyunwu/NoiseDiscrimination/data';
+% obs_name = 'xiuyun';
+% doNeq=1;
+% doEfficiency=1;
 
 %settings
 filename = [obs_name,'_runs.mat'];
@@ -19,16 +31,29 @@ cd(newpath);
 load(filename);
 % This should be the table directly saved after running parseExpData.m
 
-col_name = {'letter_size','noise_contrast','noise_decay_radius','eccentricity','mean_threshold','sd_threshold','squared_noise_contrast','mean_squared_threshod','sd_squared_threshold','Energy','Energy_at_unit_contrast','Noise_power_spectral_density'};
+col_name = {'letter_size','noise_contrast','noise_decay_radius','eccentricity','HardOrSoft','TargetCross','mean_threshold','sd_threshold','squared_noise_contrast','mean_squared_threshod','sd_squared_threshold','Energy','Energy_at_unit_contrast','Noise_power_spectral_density'};
 
 % convert conditions to positive integers so that can be used in accumarray()
 % subs is the converted condition for each run
 % each number 'n' in subs means the condition is the n_th row in con
-% the columns in con are lettersize, noisecontrast, noisedecayradius and eccentricity
-con = unique(tabdata{:, 3:6},'rows');
+% the columns in con are lettersize, noisecontrast, noisedecayradius,
+% eccentricity, noiseRadiusDeg and TargetCross
+cons = [tabdata{:, 3:6} tabdata{:, 17:18}];
+
+con = unique(cons,'rows');
 for t = 1:size(con,1)
     cont = repmat(con(t,:),[size(tabdata,1) 1]);
-    arr = find(all(tabdata{:, 3:6}==cont,2)); % the index of the two runs under current condition
+    arr = find(all(tabdata{:, 3:6}==cont(:,1:4),2));
+    if length(arr)>2 % soft/hard and cross 0/1 conditions together
+        k=1;
+        while k<=length(arr)
+            if tabdata{arr(k), 17}~=con(t,5)||tabdata{arr(k), 18}~=con(t,6)
+                arr(k)=[];
+                k=k-1;
+            end;
+            k=k+1;
+        end;
+    end; % the index of the two runs under current condition
     subs(arr,1) = t;
 end
 
@@ -53,11 +78,22 @@ if doNeq==1
     for t = 1:size(con,1)
         if con(t, 2)~=0
             cont = repmat(con(t,:),[size(tabdata,1) 1]);
-            arr = find(all(tabdata{:, 3:6}==cont,2)); % the index of the two runs
+            arr = find(all(tabdata{:, 3:6}==cont(:,1:4),2));
+            if length(arr)>2 % soft/hard and cross 0/1 conditions together
+                k=1;
+                while k<=length(arr)
+                    if tabdata{arr(k), 17}~=con(t,5)||tabdata{arr(k), 18}~=con(t,6)
+                        arr(k)=[];
+                        k=k-1;
+                    end;
+                    k=k+1;
+                end;
+            end; % the index of the two runs under current condition
+            
             
             for j = 1:size(con,1)
-                if cont(t,1)==con(j,1) && cont(t,4)==con(j,4)
-                    arr0=j; %the index of E0, whose conditon has the same letter size and eccentricity
+                if cont(t,1)==con(j,1) && cont(t,4)==con(j,4) && cont(t,6)==con(j,6)
+                    arr0=j; %the index of E0, whose conditon has the same letter size,eccentricity,and TargetCross
                     break
                 end;
             end;
@@ -77,31 +113,55 @@ end;
 if doEfficiency==1
     % computing high noise Efficiency = E_ideal/(E-E0)
     ideal = load('ideal_runs.mat');
-    runEffi = zeros(size(tabdata,1),1); % Efficiency for each run(when noise contrast = 0, Efficiency = 0)
+    runEffi = zeros(size(ideal.tabdata,1),1); % Efficiency for each run(when noise contrast = 0, Efficiency = 0)
     
-    coni = unique(ideal.tabdata{:, 3:6},'rows'); % sort the ideal data
-for t = 1:size(coni,1)
-    conti = repmat(coni(t,:),[size(ideal.tabdata,1) 1]);
-    arri = find(all(ideal.tabdata{:, 3:6}==conti,2)); % the index of the two runs under current condition
-    subsi(arri,1) = t;
-end
+    conis = [ideal.tabdata{:, 3:6} ideal.tabdata{:, 17:18}];
+    coni = unique(conis,'rows'); % sort the ideal data
+    for t = 1:size(coni,1)
+        conti = repmat(coni(t,:),[size(ideal.tabdata,1) 1]);
+        arri = find(all(ideal.tabdata{:, 3:6}==conti(:, 1:4),2)); % the index of the two runs under current condition
+        
+        if length(arri)>2 % soft/hard and cross 0/1 conditions together
+            k=1;
+            while k<=length(arri)
+                if ideal.tabdata{arri(k), 17}~=coni(t,5)||ideal.tabdata{arri(k), 18}~=coni(t,6)
+                    arri(k)=[];
+                    k=k-1;
+                end;
+                k=k+1;
+            end;
+        end; % the index of the two runs under current condition
+        subsi(arri,1) = t;
+    end
     
     meffi = accumarray(subsi, ideal.tabdata{:, 16}, [], @mean); % mean of ideal energy
     for t = 1:size(con,1)
         if con(t, 2)~=0
             cont = repmat(con(t,:),[size(tabdata,1) 1]);
-            arr = find(all(tabdata{:, 3:6}==cont,2)); % the index of the two runs
+            arr = find(all(tabdata{:, 3:6}==cont(:,1:4),2));
+            if length(arr)>2 % soft/hard and cross 0/1 conditions together
+                k=1;
+                while k<=length(arr)
+                    if tabdata{arr(k), 17}~=con(t,5)||tabdata{arr(k), 18}~=con(t,6)
+                        arr(k)=[];
+                        k=k-1;
+                    end;
+                    k=k+1;
+                end;
+            end; % the index of the two runs under current condition
+            
             
             for j = 1:size(con,1)
-                if cont(t,1)==con(j,1) && cont(t,4)==con(j,4)
-                    arr0=j; %the index of E0, whose conditon has the same letter size and eccentricity
+                if cont(t,1)==con(j,1) && cont(t,4)==con(j,4) && cont(t,6)==con(j,6)
+                    arr0=j; %the index of E0, whose conditon has the same letter size,eccentricity,and TargetCross
                     break
                 end;
             end;
             cE0 = repmat(ME(arr0),[2 1]);
             
             cont2 = repmat(con(t,:),[size(coni,1) 1]);
-            arri = find(all(coni==cont2, 2)); % the index of ideal E
+            arri = find(all(coni(:,1:5)==cont2(:,1:5), 2)); % the index of ideal E
+            % has the same lettersize, noisecontrast, noisedecayradius, eccentricity and noiseRadiusDeg
             cEf = repmat(meffi(arri),[2 1]);
             
             runEffi(arr, 1)=cEf./(tabdata{arr,16}-cE0); %high noise efficiency for the two runs
@@ -109,15 +169,29 @@ end
         
     end;
     
-        MEffi = accumarray(subs, runEffi, [], @mean);
-        SDEffi = accumarray(subs, runEffi, [], @std);
+    MEffi = accumarray(subs, runEffi, [], @mean);
+    SDEffi = accumarray(subs, runEffi, [], @std);
     
-        out = [out,MEffi,SDEffi];
-        col_name = {col_name{:}, 'mean_Efficiency','sd_Efficiency'};
+    out = [out,MEffi,SDEffi];
+    col_name = {col_name{:}, 'mean_Efficiency','sd_Efficiency'};
     
 end;
 
 out = mat2cell(out,ones(1,size(out,1)),ones(1,size(out,2)));
+
+for t = 1:size(out,1)
+    if out{t, 5}>17 % soft conditions
+out{t,5} = 'soft';
+    else
+        out{t,3}=floor(out{t,5}); 
+        % just to make sure the radius is 1... out{t,3} should equal to
+        % out{t,5}, but the value in o is a little bit larger than been
+        % typed...
+        out{t,5}='hard';
+end;
+% convert noise decay radius and noisedecayradius to noise decay radius in
+% display and soft/hard, so it would be easier when plotting
+
 tab = cell2table(out, 'VariableNames', col_name); %converts to table
 
 writetable(tab, csvfilename ,'Delimiter',',')
--- parseExpData.m
+++ parseExpData.m
@@ -1,108 +1,118 @@
-  %from_date/to_date of form 'dd-mmm-yyyy hh:mm:ss'.
-  %Put from_date = -Inf and/or to_date = Inf when wanting unrestricted
-  %lower/upper bound.
-  
+%from_date/to_date of form 'dd-mmm-yyyy hh:mm:ss'.
+%Put from_date = -Inf and/or to_date = Inf when wanting unrestricted
+%lower/upper bound.
+
 function tabdata = parseExpData(newpath, obs_name, from_date, to_date)
 
-  % settings
-  % I didn't include them into the function parameters since these may not
-  % change everytime
-  smallestTrialNum = 80; %used for skipping unfinished runs
-  csvfilename=[obs_name,'_runs.csv'];
-  matfilename=[obs_name,'_runs.mat'];
-  %settings end
+% settings
+% I didn't include them into the function parameters since these may not
+% change everytime
+smallestTrialNum = 80; %used for skipping unfinished runs
+csvfilename=[obs_name,'_runs.csv'];
+matfilename=[obs_name,'_runs.mat'];
+%settings end
 
-  cd(newpath);
-  files = dir('study*.mat');
-  filenames = {files.name};
-  filedates=cell(size(filenames));
-  filedates2=cell(size(filenames));
-  fmt = 'dd-mmm-yyyy HH:MM:SS';
-  for i=1:length(filenames)
+cd(newpath);
+files = dir('study*.mat');
+filenames = {files.name};
+filedates=cell(size(filenames));
+filedates2=cell(size(filenames));
+fmt = 'dd-mmm-yyyy HH:MM:SS';
+for i=1:length(filenames)
     temp=strsplit(filenames{i},'.');
     filedates{i}=datestr(cellfun(@str2num,temp(2:7)),0);
     filedates2{i}=datenum(filedates{i},fmt);
-  end
-  if sum(from_date==-Inf)==1
+end
+if sum(from_date==-Inf)==1
     from_dt=-Inf;
-  else
+else
     from_dt = datenum(from_date,fmt);
-  end
-  if sum(to_date==Inf)==1
+end
+if sum(to_date==Inf)==1
     to_dt=Inf;
-  else
+else
     to_dt = datenum(to_date, fmt);
-  end
+end
 
-  parsefiles=cell(1,length(filenames));
-  parsedates=cell(1,length(filenames));
-  for k=1:length(filenames)
+parsefiles=cell(1,length(filenames));
+parsedates=cell(1,length(filenames));
+for k=1:length(filenames)
     [pathstr,name,ext]=fileparts(char(filenames(k)));
     if not(isempty(strfind(name,char(obs_name))))
-      curr_date = filedates2{k};
-      if curr_date>=from_dt && curr_date<=to_dt
-        parsefiles{k}=char(filenames(k));
-        parsedates{k}=char(filedates(k));
-      end
+        curr_date = filedates2{k};
+        if curr_date>=from_dt && curr_date<=to_dt
+            parsefiles{k}=char(filenames(k));
+            parsedates{k}=char(filedates(k));
+        end
     end
-  end
-  parsefiles=parsefiles(~cellfun('isempty',parsefiles)); %command to remove empty cells
-  parsedates=parsedates(~cellfun('isempty',parsedates));
-  % pdata=cell(length(parsefiles),13);
-  pdata = {};
-  col_names={'observer','trials','letter_size','noise_contrast','noise_decay_radius','eccentricity','p_accuracy','threshold_log_contrast','threshold_contrast_SD','contrast','log_E_by_N','efficiency','file_date_time','energy_at_unit_contrast','noise_power_spectral_density', 'threshold_energy'};
+end
+parsefiles=parsefiles(~cellfun('isempty',parsefiles)); %command to remove empty cells
+parsedates=parsedates(~cellfun('isempty',parsedates));
+% pdata=cell(length(parsefiles),13);
+pdata = {};
+col_names={'observer','trials','letter_size','noise_contrast','noise_decay_radius','eccentricity','p_accuracy','threshold_log_contrast','threshold_contrast_SD','contrast','log_E_by_N','efficiency','file_date_time','energy_at_unit_contrast','noise_power_spectral_density', 'threshold_energy','noiseRadiusDeg','TargetCross'};
 
-  j = 1; % to skip the re-run runs(unfinished runs due to mis-representation of letters)
-  for i=1:length(parsefiles)
+j = 1; % to skip the re-run runs(unfinished runs due to mis-representation of letters)
+for i=1:length(parsefiles)
     load(char(parsefiles(i)),'o');
     if o.trials>=smallestTrialNum
-      pdata{j,1}=o.observer; %observer name
-      pdata{j,2}=o.trials; %trials per run
-      pdata{j,3}=o.targetHeightDeg; %letter size
-      pdata{j,4}=o.noiseSD; %noise contrast
-      pdata{j,5}=o.noiseEnvelopeSpaceConstantDeg; %noise decay radius
-      pdata{j,6}=o.eccentricityDeg; %eccentricity
-      %pdata(j,)=o.; %seconds per trials
-      pdata{j,7}=o.p; %percentage accuracy
-      pdata{j,8}=o.questMean; %threshold log contrast
-      pdata{j,9}=o.questSd; %threshold log contrast SD
-      pdata{j,10}=o.contrast; %contrast
-      pdata{j,11}=log(o.EOverN); %log E/N
-      pdata{j,12}=o.efficiency; %efficiency
-      pdata{j,13}=char(parsedates(i)); %file date for reference
-
-      % the following commands might be used for all conditions later
-      % when E1 and N are saved into o using the modified NoiseDiscrimination.m
-      if o.noiseSD==0 %for now just the new noise contrast=0 runs saved E1 and N into struct o
-        pdata{j,14}=o.E1; % energy at unit contrast
-        pdata{j,15}=o.N; % noise power spectral density
-        E1 = o.E1;
-        N = o.N;
-      end;
-
-      % for old data with noise contrast~=0 we just compute E1 and N from o
-      if o.noiseSD~=0
-        N = (o.noiseSD^2)*o.noiseCheckDeg^2;
-        E=o.EOverN*N;
-        E1=E/o.contrast^2;
-
-        pdata{j,14}=E1; % energy at unit contrast
-        pdata{j,15}=N; % noise power spectral density
-      end;
-
-      E = E1*o.contrast^2;
-      pdata{j,16}=E; %threshold energy
-
-      j = j+1;
+        pdata{j,1}=o.observer; %observer name
+        pdata{j,2}=o.trials; %trials per run
+        pdata{j,3}=o.targetHeightDeg; %letter size
+        pdata{j,4}=o.noiseSD; %noise contrast
+        pdata{j,5}=o.noiseEnvelopeSpaceConstantDeg; %noise decay radius
+        pdata{j,6}=o.eccentricityDeg; %eccentricity
+        %pdata(j,)=o.; %seconds per trials
+        pdata{j,7}=o.p; %percentage accuracy
+        pdata{j,8}=o.questMean; %threshold log contrast
+        pdata{j,9}=o.questSd; %threshold log contrast SD
+        pdata{j,10}=o.contrast; %contrast
+        pdata{j,11}=log(o.EOverN); %log E/N
+        pdata{j,12}=o.efficiency; %efficiency
+        pdata{j,13}=char(parsedates(i)); %file date for reference
+        
+        % the following commands might be used for all conditions later
+        % when E1 and N are saved into o using the modified NoiseDiscrimination.m
+        if o.noiseSD==0 %for now just the new noise contrast=0 runs saved E1 and N into struct o
+            pdata{j,14}=o.E1; % energy at unit contrast
+            pdata{j,15}=o.N; % noise power spectral density
+            E1 = o.E1;
+            N = o.N;
+        end;
+        
+        % for old data with noise contrast~=0 we just compute E1 and N from o
+        if o.noiseSD~=0
+            N = (o.noiseSD^2)*o.noiseCheckDeg^2;
+            E=o.EOverN*N;
+            E1=E/o.contrast^2;
+            
+            pdata{j,14}=E1; % energy at unit contrast
+            pdata{j,15}=N; % noise power spectral density
+        end;
+        
+        E = E1*o.contrast^2;
+        pdata{j,16}=E; %threshold energy
+        
+        pdata{j,17}=o.noiseRadiusDeg; % hard/soft
+        % when o.noiseRadiusDeg=1 & decay radius=inf, hard 1 radius;
+        % when o.noiseRadiusDeg=inf & decay radius=1, soft 1 radius
+        
+        if isfield(o,'targetCross')
+            pdata{j,18}=o.targetCross;
+        else
+            pdata{j,18}=0;
+        end;
+        
+        j = j+1;
     end;
     clear o;
-  end
-  pdata( all(cellfun(@isempty,pdata),2), : ) = []; %removes empty cell rows
-  tabdata = cell2table(pdata, 'VariableNames', col_names); %converts to table
+end
+pdata( all(cellfun(@isempty,pdata),2), : ) = []; %removes empty cell rows
+tabdata = cell2table(pdata, 'VariableNames', col_names); %converts to table
 
-  %Use this command to save file -
-  writetable(tabdata, csvfilename,'Delimiter',',')
+%Use this command to save file -
+writetable(tabdata, csvfilename,'Delimiter',',')
 
-  %Also save a .mat file for further processing
-  save(matfilename,'tabdata');
+%Also save a .mat file for further processing
+save(matfilename,'tabdata');
 end
