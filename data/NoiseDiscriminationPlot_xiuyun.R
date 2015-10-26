ed48ff9f9f1e9a42600a11e618e75d5c9214006e
CaptainS5 <xw1106@nyu.edu>
2015-10-26 13:29:51 -0400

    Efficiency vs. Eccentricity

--- data/NoiseDiscriminationPlot_xiuyun.R
+++ data/NoiseDiscriminationPlot_xiuyun.R
@@ -23,7 +23,7 @@ T$letter_size = replace(T$letter_size, T$letter_size==5.985890625,6)
 T$letter_size = replace(T$letter_size, T$letter_size==3.438703125,3.4641)
 T$RadiusRelative2letter_size = round(T$noise_decay_radius / T$letter_size, digits = 2)
 T$shiftedSquaredNoiseContrast = T$squared_noise_contrast +0.005
-#T$eccentricity = T$eccentricity - 0.45
+T$shiftedEccentricity = T$eccentricity + 1
 #noise_decay_radius = factor(noise_decay_radius, c(1,2,3,5,6,8,9,16))
 
 
@@ -117,6 +117,22 @@ pp[[4]] <- ggplot(subset(T, T$noise_contrast!=0), aes(x=RadiusRelative2letter_si
   noGrid+
   labs(title = "LetterSize 2/3.4641/6, Gaussian pink", x = "RadiusRelative2LetterSize", y = "Neq")+
   facet_grid(letter_size ~.)+
+  theme(text=element_text(size=32))
+
+xbreaks = c(0.5, 1, 2, 3, 6, 9, 16, 32)
+ybreaks = c(0.01,0.05, 0.1, 0.15, 0.2)
+ylimits = aes(ymax = mean_Efficiency + sd_Efficiency, ymin=mean_Efficiency - sd_Efficiency)
+pp[[5]] <- ggplot(subset(T, T$letter_size==2 & T$noise_contrast==0.16), aes(x=shiftedEccentricity, y=mean_Efficiency, color=factor(noise_decay_radius), size=factor(HardOrSoft), shape=factor(TargetCross) )) +
+  geom_errorbar(ylimits)+
+  geom_line()+
+  geom_point(size=12, alpha=0.6)+
+  scale_y_log10(breaks=ybreaks)+
+  scale_x_log10(breaks=xbreaks)+
+  # scale_linetype_manual(values=c("solid", "longdash", "dotted"))+
+  scale_size_discrete(range=c(0.5,2)) +
+  #scale_color_brewer(type="div",  palette = 7)
+  noGrid+
+  labs(title = "LetterSize 2, NoiseContrast 0.16, Gaussian pink", x = "shiftedEccentricity(deg)", y = "Efficiency")+
   theme(text=element_text(size=32))
 
 # xbreaks = c(0.01,0.02,0.03,0.04, 0.05)
