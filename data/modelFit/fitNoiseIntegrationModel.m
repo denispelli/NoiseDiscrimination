a1de4704af7d1dff49a96973b5c02ce84efebf1e
Nick Blauch <nblauch@icloud.com>
2015-11-02 22:31:38 -0500

    Updates to model

--- data/modelFit/fitNoiseIntegrationModel.m
+++ data/modelFit/fitNoiseIntegrationModel.m
@@ -11,26 +11,26 @@ function [thresholdContrastFit,offset,scale,integrationRadius] = fitNoiseIntegra
     count = 0;
     for i = 1:length(userThresholds)
         if noiseSD(i) == 0 || userRadii(i) == 0
-            Sum = Sum + userThresholds(i);
+            Sum = Sum + userThresholds(i).^2;
             count = count + 1;
         end
     end
     offset = Sum/count;
     
-    intRadii = 0:.1:4; %integration radii to test
+    intRadii = 0:.1:3; %integration radii to test
     scalars = 0:.01:1; %scale factors to test
     %initializing variables
-    contrastFit = zeros(length(userThresholds),length(intRadii));
-    contrast = zeros(length(userThresholds),length(scalars));
+    integratedPower = zeros(length(userThresholds),length(intRadii));
+    squaredContrast = zeros(length(userThresholds),length(scalars));
     fitParameters = zeros(2,length(intRadii));
     %test a range of integration radii. for each radius tested, find the
     %optimal scale factor. save as a pair.
     for index=1:length(intRadii)
-        contrastFit(:,index) = computeContrast(intRadii(index),userRadii,noiseSD);
+        integratedPower(:,index) = integratePower(intRadii(index),userRadii,noiseSD);
         SE = zeros(length(scalars),1);
         for index2 = 1:length(scalars)
-            contrast(:,index2) = offset + scalars(index2)*contrastFit(:,index);
-            SE(index2,1) = sum((userThresholds(:) - contrast(:,index2)).^2);
+            squaredContrast(:,index2) = scalars(index2).*(offset + integratedPower(:,index));
+            SE(index2,1) = sum((userThresholds(:).^2 - squaredContrast(:,index2)).^2);
         end
         
         [minVal,indexOfMin] = min(SE);
@@ -42,15 +42,15 @@ function [thresholdContrastFit,offset,scale,integrationRadius] = fitNoiseIntegra
     %minimizes squared-error when compared against user input data.
     SE2 = zeros(length(intRadii),1);
     for index = 1:length(intRadii)
-        finalContrast(:,index) = offset + fitParameters(1,index)*contrastFit(:,index);
-        SE2(index) = sum((userThresholds(:) - finalContrast(:,index)).^2);
+        finalSquaredContrast(:,index) = fitParameters(1,index).*(offset + integratedPower(:,index));
+        SE2(index) = sum((userThresholds(:).^2 - finalSquaredContrast(:,index)).^2);
     end
     [minVal,indexOfMin] = min(SE2);
     
     %Return the best-fit parameters
     scale = fitParameters(1,indexOfMin);
     integrationRadius = fitParameters(2,indexOfMin);
-    thresholdContrastFit = offset + scale*computeContrast(integrationRadius,userRadii,noiseSD);
+    thresholdContrastFit = sqrt(offset + scale*integratePower(integrationRadius,userRadii,noiseSD));
     
 
 end
