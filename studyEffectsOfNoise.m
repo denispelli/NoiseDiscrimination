c36ccfc292c45d87423976aceb3631f1377bdc3e
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-11-20 19:27:41 -0500

    shell for gabor

--- studyEffectsOfNoise.m
+++ studyEffectsOfNoise.m
@@ -4,7 +4,7 @@
 
 
 %#### Adjust values within this block of code #####################
-o.observer='ideal'; %add your name here
+o.observer='hyiltiz'; %add your name here
 o.distanceCm=50; % viewing distance
 o.durationSec=0.2;
 
@@ -19,22 +19,34 @@ o.durationSec=0.2;
 o.noiseRadiusDeg=inf; % change this to manipulate noise decay radius [1,sqrt(3), 3,3*sqrt(3), 9, Inf]
 o.noiseEnvelopeSpaceConstantDeg=Inf; % always Inf for hard edge
 
-o.targetHeightDeg=2; %letter size [2,sqrt(2*6),6];
-o.eccentricityDeg=32; % eccentricity [0,32]
+o.targetHeightDeg=6; %letter size [2,sqrt(2*6),6];
+o.eccentricityDeg=0; % eccentricity [0,32]
 o.noiseSD=0.16; %noise contrast [0.16]
 
 o.noiseType='gaussian'; % ALWAYS use gaussian
 o.noiseSpectrum='white'; % pink or white
 o.targetCross=1;
 o.fixationCrossWeightDeg = 0.05; % target line thickness
+
+
+o.targetGaborPhaseDeg=0; % Phase offset of sinewave in deg at center of gabor.
+o.targetGaborSpaceConstantCycles=0.75; % The 1/e space constant of the gaussian envelope in cycles of the sinewave.
+o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
+o.targetGaborOrientationsDeg=[0 90]; % Orientations relative to vertical.
+
 %##################################################################
 
 
 %#########################################
 o.noiseCheckDeg=o.targetHeightDeg/10;
 % o.isWin=0; % use the Windows code even if we're on a Mac
+o.targetKind='gabor';
+% o.targetKind='gabor'; % one cycle within targetSize
+o.targetGaborNames='VH';
 o.task='identify';
-o.signalKind='luminance'; % Display a luminance decrement.
+% o.targetModulates='noise';  % Display a noise increment.
+o.targetModulates='luminance'; % Display a luminance decrement.
+% o.targetModulates='entropy'; % Display an entropy increment.
 
 % o.noiseRaisedCosineEdgeThicknessDeg=0; % midpoint of raised cosine is at o.noiseRadiusDeg.
 % o.durationSec=inf; % Typically 0.2 or inf (wait indefinitely for response).
@@ -52,3 +64,6 @@ o.isKbLegacy = 0; % Uses KbWait, KbCheck, KbStrokeWait functions, instead of Get
 o.trialsPerRun=3000;
 o=NoiseDiscrimination(o);
 sca;
+
+
+
