aa07139625f830a0f48b8a619dd796941152cbb3
denispelli <denis.pelli@nyu.edu>
2015-11-21 05:36:36 -0500

    rename checkResponse to GetKeypress
    Enhanced to return just the number when you press a number key.

--- lib/GetKeypress.m
+++ lib/GetKeypress.m
@@ -1,10 +1,15 @@
-function response = checkResponse(isKbLegacy)
-    while KbCheck;end
-    response = 0;
+function response = GetKeypress(isKbLegacy)
+% Wait for keypress, and return the lowercase character, e.g. 'a' or '4',
+% or key name, e.g. 'left_shift'.
+%
+% Originally called "checkResponse" written by Hörmet Yiltiz, October 2015.
+% Renamed "GetKeypress" by Denis Pelli, November 2015.
     if nargin == 0
         isKbLegacy = 0;
     end
-
+    while KbCheck
+    end
+    response=0;
     if isKbLegacy
         ListenChar(0); % flush
         ListenChar(2); % no echo
@@ -13,9 +18,19 @@ function response = checkResponse(isKbLegacy)
         ListenChar; % normal
     else
         % use modern Kb* functions
-        [secs, keyCode] = KbStrokeWait(); % we only need keyIsDown and keyCode
+        [secs, keyCode] = KbStrokeWait(); % we only need keyCode
         response = KbName(keyCode);
         %disp(sprintf('0:==>%s<==', response));
+        
+        % KbName, used by checkResponse, returns 2 characters, e.g. '0)',
+        % when you press a number key. So we use only the first character
+        % of the string returned by checkResponse. This ignores the state
+        % of the shift key, assuming no shift. We intentionally do not
+        % distinguish between a number key on a number pad and a number key
+        % on the main keyboard.
+        if length(response)==2
+            response=response(1);
+        end
 
         if ismember(response, {'period', '.>', '.'}); response = '.'; end
         %disp(sprintf('1:==>%s<==', response));
