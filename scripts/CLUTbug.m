121f24975578011928e6b920a78e0d8068afe44e
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2015-12-21 17:05:13 -0500

    move scripts into scripts folder

---
+++ scripts/CLUTbug.m
@@ -0,0 +1,11 @@
+window=Screen('OpenWindow',0,255);
+gamma=Screen('ReadNormalizedGammaTable',window);
+fprintf('gamma size %dx%d\n',size(gamma));
+gamma(37,1:3)=[0 1 0]; % green background of 1
+gamma(531,1:3)=[1 0 0]; % red square of 128
+Screen('LoadNormalizedGammaTable',window,gamma);
+Screen('FillRect',window,[1 1 1]); % green background of 1
+Screen('FillRect',window,[128 128 128],[0 0 200 200]); % red square of 128
+Screen('Flip',window);
+GetClicks;
+sca;
