86b54b91f453d65d8c231b4c2801783b2cd5fd11
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2016-02-23 19:34:19 -0500

    rdy for testing with pause

--- studyEffectsOfNoise.m
+++ studyEffectsOfNoise.m
@@ -1,10 +1,9 @@
 %#### Adjust values within this block #####################
 clear o
+useBackupSessions=1;
 o.observer='junk';
 % o.observer='ideal';
-% o.observer='hormet'; % use your name
 % o.observer='xiuyun'; % use your name
-% o.observer='shivam'; % use your name
 o.distanceCm=50; % viewing distance
 o.durationSec=0.2;
 o.trialsPerRun=80;
@@ -19,12 +18,12 @@ o.trialsPerRun=80;
 
 % ############# we test target size x ecc w/o noise #######
 % o.targetHeightDeg=6; % OLD: letter or gabor size [2 3.5 6];
-o.targetHeightDeg=8; % letter/gabor size [2 4 8].
-o.eccentricityDeg=0; % eccentricity [0 16 32]
+o.targetHeightDeg=2; % letter/gabor size [2 4 8].
+o.eccentricityDeg=8; % eccentricity [0 16 32]
 o.noiseSD=0.16; % noise contrast [0 0.16]
 % We want to compare these:
 o.noiseCheckDeg=o.targetHeightDeg/20;
-o.noiseCheckDeg=o.targetHeightDeg/40;
+%o.noiseCheckDeg=o.targetHeightDeg/40;
 % #########################################################
 
 % ############# We plan to test these soon #######
@@ -32,31 +31,28 @@ o.noiseCheckDeg=o.targetHeightDeg/40;
 % #########################################################
 
 % ############## Below is constant for this week ##########
-% o.targetKind='letter';
-o.targetKind='gabor'; % a grating patch
+o.targetKind='letter';
+
+%o.targetKind='gabor'; % a grating patch
 % These two sets of orientation produce the same gabors, they differ only
 % in the order in which they appear on the response screen. The first set
 % begins at 0 vertical. The second set begins at horizontal. Use whichever
 % you prefer.
-o.targetGaborOrientationsDeg=[0 30 60 90 120 150]; % Orientations relative to vertical.
-o.targetGaborOrientationsDeg=[-90 -60 -30 0 30 60]; % Orientations relative to vertical.
-o.targetGaborNames='123456'; % Observer types 1 for 0 deg, 2 for 30 deg, etc.
+%o.targetGaborOrientationsDeg=[0 30 60 90 120 150]; % Orientations relative to vertical.
+%o.targetGaborOrientationsDeg=[-90 -60 -30 0 30 60]; % Orientations relative to vertical.
+%o.targetGaborNames='123456'; % Observer types 1 for 0 deg, 2 for 30 deg, etc.
 %##########################################################
 
 % o.targetGaborOrientationsDeg=[0 90]; % Orientations relative to vertical.
 % o.targetGaborNames='VH'; % Observer types V for vertical or H for horizontal.
 
-o.targetHeightDeg=2; % letter size [2,sqrt(2*6),6];
-o.eccentricityDeg=32; % eccentricity [0,32]
-o.noiseSD=0.16; % noise contrast [0.16]
 
 %## Fixed values, for all current testing. Do not adjust. #####
 % Gaussian noise envelope: soft cut off
 % o.noiseRadiusDeg=inf;
 % noiseEnvelopeSpaceConstantDeg: 1
 
-% Top-hat noise envelope: sharp cut off
-o.noiseEnvelopeSpaceConstantDeg=Inf; % always Inf for hard edge
+o.noiseEnvelopeSpaceConstantDeg=1; % always Inf for hard edge top-hat noise
 % o.noiseRadiusDeg=inf; % noise decay radius [1 1.7 3 5.2 9 Inf]
 o.noiseRadiusDeg=inf;
 
@@ -66,14 +62,16 @@ o.targetCross=1;
 o.fixationCrossWeightDeg = 0.05; % target line thickness
 
 o.alphabetPlacement='top'; % show possible answers on 'top' or 'right' for letters and gabors.
+
+
 % o.targetKind='letter'; % use letter target
 %##################################################################
-o.targetKind='gabor'; % use gabor target. one cycle within targetSize
-o.targetGaborPhaseDeg=0; % Phase offset of sinewave in deg at center of gabor.
-o.targetGaborSpaceConstantCycles=0.75; % The 1/e space constant of the gaussian envelope in cycles of the sinewave.
-o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
-o.targetGaborOrientationsDeg=[0 90]; % Orientations relative to vertical.
-o.targetGaborNames='VH'; % "V" for vertical, and "H" for horizontal.
+%o.targetKind='gabor'; % use gabor target. one cycle within targetSize
+%o.targetGaborPhaseDeg=0; % Phase offset of sinewave in deg at center of gabor.
+%o.targetGaborSpaceConstantCycles=0.75; % The 1/e space constant of the gaussian envelope in cycles of the sinewave.
+%o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
+%o.targetGaborOrientationsDeg=[0 90]; % Orientations relative to vertical.
+%o.targetGaborNames='VH'; % "V" for vertical, and "H" for horizontal.
 % When plotting the gabor data, use either spatial frequency f in c/deg, or
 % period size A in deg.
 % f = o.targetGaborCycles/o.targetSizeDeg;
@@ -81,14 +79,14 @@ o.targetGaborNames='VH'; % "V" for vertical, and "H" for horizontal.
 % We should test the same values of o.targetHeightDeg for gabors as for
 % letters.
 %#########################################
-o.noiseCheckDeg=o.targetHeightDeg/20;
+
+
+%o.noiseCheckDeg=o.targetHeightDeg/20;
 % o.isWin=0; % use the Windows code even if we're on a Mac
-o.targetGaborPhaseDeg=90; % Phase offset of sinewave in deg at center of gabor.
-o.targetGaborSpaceConstantCycles=0.75; % The 1/e space constant of the gaussian envelope in cycles of the sinewave.
-o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
-o.task='identify';
-o.targetModulates='luminance'; % Display a luminance decrement.
-% o.isWin=0; % use the Windows code even if we're on a Mac
+%o.targetGaborPhaseDeg=90; % Phase offset of sinewave in deg at center of gabor.
+%o.targetGaborSpaceConstantCycles=0.75; % The 1/e space constant of the gaussian envelope in cycles of the sinewave.
+%o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
+%o.targetModulates='luminance'; % Display a luminance decrement.
 % o.noiseRaisedCosineEdgeThicknessDeg=0; % midpoint of raised cosine is at o.noiseRadiusDeg.
 % o.durationSec=inf; % Typically 0.2 or inf (wait indefinitely for response).
 % o.tGuess=log10(0.2); % Optionally tell Quest the initial log contrast on first trial.
@@ -102,7 +100,83 @@ o.targetModulates='luminance'; % Display a luminance decrement.
 o.speakInstructions=0;
 o.isKbLegacy = 0; % Uses KbWait, KbCheck, KbStrokeWait functions, instead of GetChar, for Linux compatibility.
 % o.useFractionOfScreen=0.3; % 0: normal, 0.5: small for debugging.
-o=NoiseDiscrimination(o);
-sca;
 
 
+if useBackupSessions % auto-generate full sequence of experiments for "Winter" data collection
+  
+  % ecc    targetSize
+  tableCell = ...
+    {0 ,   [0.5, 1, 2, 4, 8, 16];
+    8 ,   [1,   2, 4, 8, 16   ];
+    16,   [1,   2, 4, 8, 16   ];
+    32,   [2,   4, 8, 16      ];}
+  
+  NoiseDecayRaiusOverLetterRadius = [0.33, 0.58, 1.00, 1.75, 3.00, 32];
+  
+  iCounter = 1;
+  for iEcc = 1:size(tableCell,1)
+    for iTargetSize=1:numel(tableCell{iEcc,2})
+      for iRatio=0:numel(NoiseDecayRaiusOverLetterRadius)
+        oo(iCounter) = o;
+        oo(iCounter).eccentricityDeg = tableCell{iEcc,1};
+        oo(iCounter).targetHeightDeg = tableCell{iEcc,2}(iTargetSize);
+        
+        if iRatio==0
+          % no noise
+          oo(iCounter).noiseSD = 0; %override previously specified noiseSD
+          oo(iCounter).noiseEnvelopeSpaceConstantDeg = NaN;
+        else
+          % high noise; noise decay radius (noiseSD is already specified above as 0.16)
+          oo(iCounter).noiseEnvelopeSpaceConstantDeg = ...
+            NoiseDecayRaiusOverLetterRadius(iRatio).*oo(iCounter).targetHeightDeg/2;
+        end
+        
+        iCounter = iCounter + 1;
+      end
+    end
+  end
+  
+  % now shuffle
+  oo = Shuffle(oo);
+  
+  assert(numel(oo)==iCounter-1);
+  disp([num2str(iCounter) ' shuffled conditions at all eccentricies and letter sizes with noise at required decay radii and without noise have been generated in total!']);
+  disp('See https://github.com/hyiltiz/NoiseDiscrimination/wiki/How-to-collect-data#winter')
+  
+  session.matFileName=['session_' datestr(now,'yyyymmddHHMMSS') '.mat'];
+  session.progressTrialNO=1;
+  if strcmp(lower(input(sprintf('\nYou will have to create new session file normally when you change to a new observer.\nCreate a new session file with the full data structure above?  (y/n)\n'), 's')),'y');
+    save(session.matFileName, 'oo', 'session');
+  else
+    disp('Did not create a new session file');
+    disp('We can use previously created ones');
+  end
+  
+  
+  % now start running the experiments
+  sessionFiles = dir('session_*.mat');
+  if numel(sessionFiles)>1;error('More than 1 session files are found! Please backup all, then only place one under current directory.');end
+  
+  sessionFile = sessionFiles(1).name;
+  save(['backup_' datestr(now,'yyyymmddHHMMSS') '.mat']);
+  disp('A backup file is created for your current workspace. You can safely delete it if the previous experiment was successful. If not, then keep that backup.')
+  
+  load(sessionFile); % WARNING: this overrides session and oo struct! Good we always backup before loading, so no data is lost
+  progressTrialNO=session.progressTrialNO;
+  for iProgressTrialNO=progressTrialNO:numel(oo) % pick up from where we left off
+    ooWithData(iProgressTrialNO)=NoiseDiscrimination(oo(iProgressTrialNO));
+    sca;
+    if ooWithData(iProgressTrialNO).runAborted
+      break;
+      fprintf('Your previous run was not successful! \nNot saving the results into session. \nRun this file again when ready for future data collection.\n\n');
+    else
+      session.progressTrialNO=session.progressTrialNO+1;
+    end
+    save(session.matFileName, 'oo', 'session');
+    if ~strcmp(lower(input('Continue into next run? (y/n)\n', 's')),'y');break;end
+  end
+  
+else
+  o=NoiseDiscrimination(o);
+  sca;
+end
