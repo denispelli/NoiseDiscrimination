d8fe6b9ba265a9ca7a1aa80d2695e66b83ee321c
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2017-01-24 12:45:43 -0500

    let's try Bookman. Should be ready.

--- testDynamicNoise.m
+++ testDynamicNoise.m
@@ -8,27 +8,22 @@ o.trialsPerRun=40;
 % A value of 1 will cancel dynamic noise (only 1 flip of noise will be generated)
 % Actual value will depend on frame rate and stimulus presentation duration,
 % thus will be ALWAYS overwritten later for any value other than 1
-o.dynamicSignalPoolSize = 1; % or 1 for static noise
+o.dynamicSignalPoolSize = 10; % or 1 for static noise
 o.dynamicPreSignalNoisePoolDur = 0.2;
 o.dynamicPostSignalNoisePoolDur = 0.2;
 
 
-o.targetKind='letter';
+o.allowAnyFont=1;
 % o.font='Sloan';
 o.font='Bookman';
 o.targetHeightDeg=7.64; % Target size, range 0 to inf. If you ask for too much, it gives you the max possible.
-o.durationSec=0.5; % Typically 0.2 or inf (wait indefinitely for response).
 o.noiseType='binary'; % 'gaussian' or 'uniform' or 'binary'
 o.noiseSpectrum='white'; % pink or white
-o.targetCross=1;
-o.fixationCrossWeightDeg = 0.05; % target line thickness
-% o.fixationCrossBlankedNearTarget=0; % always present fixation
-o.alphabetPlacement='top'; % show possible answers on 'top' or 'right' for letters and gabors.
 o.noiseCheckDeg=0.09;
 o.noiseSD=0.5; % noise contrast [0 0.16]
 o.eccentricityDeg=0; % eccentricity [0 8 16 32]
 o.noiseEnvelopeSpaceConstantDeg=128; % always Inf for hard edge top-hat noise
-% o.noiseRadiusDeg=inf; % noise decay radius [1 1.7 3 5.2 9 Inf]
+o.noiseRadiusDeg=inf; % noise decay radius [1 1.7 3 5.2 9 Inf]
 
 %For noise with Gaussian envelope (soft)
 %o.noiseRadiusDeg=inf;
@@ -37,9 +32,8 @@ o.noiseEnvelopeSpaceConstantDeg=128; % always Inf for hard edge top-hat noise
 %For noise with tophat envelope (sharp cut off beyond disk with radius 1)
 %o.noiseRadiusDeg=1;
 %noiseEnvelopeSpaceConstantDeg: Inf
-o = NoiseDiscrimination(o);
-sca;
 
+o.targetKind='letter';
 o.targetCross=1;
 o.fixationCrossWeightDeg = 0.05; % target line thickness
 % o.fixationCrossBlankedNearTarget=0; % always present fixation
@@ -58,3 +52,6 @@ o.snapshotShowsFixationAfter=0;
 o.speakInstructions=0;
 o.isKbLegacy = 0; % Uses KbWait, KbCheck, KbStrokeWait functions, instead of GetChar, for Linux compatibility.
 % o.useFractionOfScreen=0.3; % 0: normal, 0.5: small for debugging.
+
+o = NoiseDiscrimination(o);
+sca;
