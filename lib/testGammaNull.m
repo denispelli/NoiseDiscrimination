63b28569b5f91f4c111598b198a7cda5d12776f7
denispelli <denis.pelli@nyu.edu>
2017-02-21 05:41:54 -0500

    added staircase at bottom

--- lib/testGammaNull.m
+++ lib/testGammaNull.m
@@ -1,5 +1,5 @@
 % Test my linearization software and luminance calibration by displaying a
-% 2 c/deg grating made up alternating stripes. One stripe has alternating
+% 3 c/deg grating made up alternating stripes. One stripe has alternating
 % pixels of white and black. The other stripe is a uniform gray selected,
 % from prior photometry, to have a luminance equal to the average of white
 % and black.
@@ -20,30 +20,46 @@ try
     periodPix=pixPerDeg/spatialFrequencyCyclesPerDeg;
     periodPix=2*round(periodPix/2);
     cal=OurScreenCalibrations(0);
-    cal.LFirst=min(cal.old.L);
-    cal.LLast=max(cal.old.L);
-    LMean=(cal.LFirst+cal.LLast)/2;
     cal.nFirst=2;
     cal.nLast=254;
+    cal.LFirst=min(cal.old.L);
+    cal.LLast=max(cal.old.L);
     cal=LinearizeClut(cal);
+    Screen('LoadNormalizedGammaTable',0,cal.gamma);
+    WaitSecs(0.1);
+    [cal.loadedGamma,dacBits,realLUTSize] = Screen('ReadNormalizedGammaTable',0);
+    LMean=(cal.LFirst+cal.LLast)/2;
     width=RectWidth(screenRect);
-    imageL=cal.LFirst*ones(1,width);
-    odd=rem(1:width,2)==1;
-    imageL(odd)=cal.LLast;
-    image=IndexOfLuminance(cal,imageL);
-    imageEstimate=EstimateLuminance(cal,image);
+    height=RectHeight(screenRect);
+    imageL=[cal.LFirst cal.LLast;LMean LMean]
+    imageI=IndexOfLuminance(cal,imageL)
+    imageEstimate=EstimateLuminance(cal,imageI)
+    image=repmat(imageI,ceil(height/2/periodPix),ceil(width/2));
+    image=Expand(image,1,periodPix);
+    image=image(1:height,1:width);
     [w,screenRect]=Screen('OpenWindow',screen,255,[],[],[],[],[],kPsychNeedRetinaResolution);
     texture=Screen('MakeTexture',w,image);
-    Screen('DrawTexture',w,texture,RectOfMatrix(image),screenRect);
+    Screen('DrawTexture',w,texture);
     Screen('Close',texture);
-    stripeRect=screenRect;
-    stripeRect(4)=periodPix/2;
-    m=IndexOfLuminance(cal,LMean);
-    while ~IsEmptyRect(ClipRect(stripeRect,screenRect))
-        Screen('FillRect',w,m,stripeRect);
-        stripeRect=OffsetRect(stripeRect,0,2*RectHeight(stripeRect));
+    nSteps = 8;
+    rampHeight = ceil(height*2/3);
+    rampL = linspace(cal.LFirst,cal.LLast,nSteps);
+    ramp = reshape(repmat(rampL, [width/nSteps 1]), 1, []);
+    ramp = repmat(ramp, [rampHeight 1]);
+    ramp = IndexOfLuminance(cal, ramp);
+    texture=Screen('MakeTexture',w,ramp);
+    rampRect = screenRect;
+    rampRect(2) = rampRect(4)-rampHeight;
+    Screen('DrawTexture',w,texture,RectOfMatrix(ramp),rampRect);
+    Screen('Close',texture);
+    for i=1:nSteps
+        if i==1
+            color=255;
+        else
+            color=0;
+        end
+        Screen('DrawText',w,sprintf('%.1f cd/m^2',rampL(i)),(-0.8+i)*width/nSteps, height-40,color);
     end
-    Screen('LoadNormalizedGammaTable',0,cal.gamma);
     Screen('Flip',w);
     Speak('Click to quit');
     GetClicks;
