4fa049d9a57e12a049cadd3a7739cb10b92e65c9
denispelli <denis.pelli@nyu.edu>
2017-02-25 14:47:04 -0500

    error check for missing args

--- lib/ffprintf.m
+++ lib/ffprintf.m
@@ -1,6 +1,9 @@
 function [count] = ffprintf(fid,varargin)
 %FFPRINTF is an enhanced fprintf that allows fid to be an array
-%   Denis Pelli, NYU, March 24, 2015
+%   Denis Pelli, NYU, March 24, 2015, 2017
+if nargin<2
+   error('You must provide two arguments: the fid and a string.');
+end
 for f=fid
     count=fprintf(f,varargin{:});
 end
