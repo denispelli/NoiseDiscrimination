b2d2f7d427d6da09becd8dbd51d285ade4da290a
denispelli <denis.pelli@nyu.edu>
2017-06-18 12:38:26 -0400

    Copied from CriticalSpacing
    This is a substantial better version. It supports all keyboards. Default is to return one character.

--- lib/GetKeypress.m
+++ lib/GetKeypress.m
@@ -1,49 +1,65 @@
-function response = GetKeypress(isKbLegacy,enableKeys)
-% Wait for keypress, and return the lowercase character, e.g. 'a' or '4',
-% or the key name, e.g. 'left_shift' or 'escape'. We do not distinguish
-% between pressing a number key on the main or separate numeric keyboard;
-% we just return the one-digit number as a character.
+function response = GetKeypress(enableKeys,deviceIndex,returnOneChar)
+% response = GetKeypress(enableKeys,deviceIndex,returnOneChar); 
+% Wait for a keypress, and return it. If returnOneChar is true (default)
+% then "response" is just one character, if possible. (Some keynames, like
+% 'left_shift', have no obviously associated character and are passed
+% through.) This does not distinguish between pressing a number key on the
+% main or separate numeric keyboard. If returnOneChar is false, then the
+% full descriptive output of KbName is returned, unmodified, e.g. 'a',
+% '1!', 'ESCAPE', or 'left_shift'.
 %
-% On June 18, 2017, I'm seeing inconsistent capitalization from KbName.
-% It's my impression that earlier tonight the escape character was reported
-% as 'escape' but now it's reported as 'ESCAPE'. Current RETURN is reported
-% as 'Return'. My fix is to force everything to lowercase.
-%
-% Originally called "checkResponse" written by Hörmet Yiltiz, October 2015.
-% Renamed "GetKeypress" by Denis Pelli, November 2015.
-if nargin == 0
-   isKbLegacy = 0;
-end
-restrictKeys=nargin>=2;
-while KbCheck
-end
-response=0;
-if isKbLegacy
-   ListenChar(0); % flush
-   ListenChar(2); % no echo
-   response=GetChar;
-   ListenChar(0); % flush
-   ListenChar; % normal
+% First version written by Hormet Yiltiz, October 2015, as "checkResponse".
+% Renamed "GetKeypress" by Denis Pelli, November 2015, and enhanced.
+
+printLog = 0;
+if nargin >= 1
+   % enableKeys should be cell strings.
+   restrictKeys=1;
+   oldEnableKeys=RestrictKeysForKbCheck(enableKeys);
+   if printLog; disp('Enabled keys list is:'); disp(enableKeys); end
 else
-   if restrictKeys
-      oldEnablekeys = RestrictKeysForKbCheck(enableKeys);
-   end
-   [~, keyCode] = KbStrokeWait();
-   if restrictKeys
-      RestrictKeysForKbCheck(oldEnablekeys);
-   end
-   response = KbName(keyCode);
+   restrictKeys=0;
+end
+if nargin<2
+   % Accept input from all keyboards and keypads.
+   deviceIndex=-3;
+end
+if nargin<3
+   % By default, simulate the behavior of GetChar(), i.e. return an ASCII
+   % code corresponding to the key pressed. That's tricky. Each key is in
+   % principle associated with a different ASCII code when shifted, but we
+   % ignore the shift. Also some ASCII codes (e.g. '1') are associated with
+   % multiple keys (on main keyboard and numeric keypad). And some keys
+   % (e.g. shift) have no ASCII code. We convert some long character names,
+   % e.g. 'escape', back to the single ASCII code. When KbName returns two
+   % characters, e.g. for the '1!' key, Return only the initial character,
+   % discarding the second,
+   returnOneChar = 1;
+end
+KbName('UnifyKeyNames');
+while KbCheck; end
+[~,keyCode] = KbStrokeWait(deviceIndex);
+response = KbName(keyCode);
+if printLog;fprintf('You pressed "%s", ',response);end
+if returnOneChar
    response=lower(response);
-   % fprintf('GETKEYPRESS: KbName ''%s''\n',response);
-   
-   % KbName, used by checkResponse, returns 2 characters, e.g. '0)',
-   % when you press a number key on the main keyboard. So when KbName
-   % returns two characters, we use only the first. This ignores the
-   % state of the shift key, assuming no shift. Thus we do not
-   % distinguish between a number key on a number pad and a number key
+   if streq(response,'space'); response=' '; end
+   if streq(response,'escape'); response=char(27); end
+   if streq(response,'return'); response=char(13); end
+   % We assume that only one key is pressed (no shift, caps lock, etc.).
+   % For keys in the upper row of the keyboard, including the number keys,
+   % KbName returns 2 characters, e.g. '0)'. So when KbName returns two
+   % characters, we return the first and discard the second. Thus we do
+   % not distinguish between a number key on a number pad and a number key
    % on the main keyboard.
    if length(response)==2
       response=response(1);
    end
+else
+   % Pass through the unmodified output from KbName.
+end
+if printLog;fprintf('and we returned "%s".\n', response); end
+if restrictKeys
+   RestrictKeysForKbCheck(oldEnableKeys);
 end
 end
