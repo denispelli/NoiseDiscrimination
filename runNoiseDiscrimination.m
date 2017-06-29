ad125ee1203ddbd9869d247150ebc82bcb187f73
denispelli <denis.pelli@nyu.edu>
2017-06-29 02:05:47 -0400

    trivial

---
+++ runNoiseDiscrimination.m
@@ -0,0 +1,12 @@
+clear o
+o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
+o.eccentricityXYDeg=[-5 5];
+o.markTargetLocation=1;
+o.targetHeightDeg=4;
+o.useDynamicNoiseMovie = 1;
+o.moviePreSec = 0.3;
+o.moviePostSec = 0.3;
+o.targetMarkDeg=1;
+o.fixationCrossDeg=3;
+o.blankingRadiusDeg=0;
+o=NoiseDiscrimination(o);
