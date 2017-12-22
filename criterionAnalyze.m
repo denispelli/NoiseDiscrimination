ab3fb46db346ce46f5ddb5c07fa25c86a61db360
denispelli <denis.pelli@nyu.edu>
2017-12-22 02:24:47 -0500

    add caption

--- criterionAnalyze.m
+++ criterionAnalyze.m
@@ -1,4 +1,4 @@
-% Analyze the data collected by checkSizeRun.
+%% Analyze the data collected by <experiment>Run.
 experiment='criterion';
 dataFolder=fullfile(fileparts(mfilename('fullpath')),'data');
 cd(dataFolder);
@@ -28,14 +28,14 @@ data = data([data(:).trials]>=40); % Discard thresholds with less than 40 trials
 fprintf('%d thresholds.\n',length(data));
 assert(~isempty(data))
 
-% Compute derived quantities
+%% Compute derived quantities
 for i=1:2:length(data)
    % Neq=N E0/(E-E0)
    data(i).E0=data(i+1).E;
    data(i).Neq=data(i).N*data(i).E0*(data(i).E-data(i).E0);
 end
 
-% Create CSV file
+%% Create CSV file
 t=struct2table(data);
 spreadsheet=fullfile(fileparts(mfilename('fullpath')),'data',[experiment '.csv']);
 writetable(t,spreadsheet);
@@ -44,9 +44,12 @@ fprintf('All selected fields for thresholds with at least 40 trials have been sa
 
 fprintf('Please make a log-lin plot of Neq vs. pThreshold.\n');
 
-% Plot
+%% Plot
 figure;
 semilogy([data([1:2:end]).pThreshold],[data([1:2:end]).pThreshold],'-');
 title(experiment);
 xlabel('pThreshold');
 ylabel('Neq (sec deg^2)');
+caption=sprintf('experimenter %s, observer %s, targetKind %s, noiseType %s',...
+   data(1).experimenter,data(1).observer,data(1).targetKind,data(1).noiseType);
+annotation('textbox',[.1 0 1 1],'String',caption,'FitBoxToText','on');
