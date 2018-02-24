e7d0314771384a15035554667ac481bbb31c1a9b
denispelli <denis.pelli@nyu.edu>
2018-02-24 15:56:31 -0500

    test flanker code

--- flankersRun.m
+++ flankersRun.m
@@ -39,24 +39,27 @@ o.targetCyclesPerDeg=3;
 o.targetGaborCycles=3;
 o.targetHeightDeg=o.targetGaborCycles/o.targetCyclesPerDeg;
 o.noiseCheckDeg=o.targetHeightDeg/20;
-o.useFlankers=true;
-o.flankerContrast=-0.85; % Negative for dark letters.
-% o.flankerContrast=nan; % Nan requests that flanker contrast always equal signal contrast.
-o.flankerSpacingDeg=4;
-      o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 
 cal=OurScreenCalibrations(0);
-if ~streq(cal.macModelName,'MacBookPro14,3') && false
+if false && ~streq(cal.macModelName,'MacBookPro14,3')
    % For debugging, if this isn't a 15" MacBook Pro 2017, pretend it is.
    cal.screenWidthMm=330; % 13"
    cal.screenHeightMm=206; % 8.1"
    warning('PRETENDING THIS IS A 15" MacBook Pro 2017');
 end
 
+o.useFlankers=true;
+o.thresholdParameter='flankerContrast';
+o.contrast=0.2;
+o.flankerContrast=-0.85; % Negative for dark letters.
+% o.flankerContrast=nan; % Nan requests that flanker contrast always equal signal contrast.
+o.flankerSpacingDeg=3;
+%       o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 % Two noise levels, noiseSD: 0 0.16
 o.experiment='flankers';
 o.conditionName='cortical';
-o.eccentricityXYDeg=[0 0];
+o.eccentricityXYDeg=[18 0];
+o.targetHeightDeg=2;
 o.targetDurationSec=2;
 o.desiredLuminance=[];
 o.desiredLuminanceFactor=1;
@@ -64,7 +67,6 @@ o.desiredLuminanceFactor=1;
 o.eyes='right';
 % for noiseSD=Shuffle([0 0.16])
 for noiseSD=[.06 0]
-   o.targetHeightDeg=4;
    %          o.minScreenWidthDeg=1+abs(o.eccentricityXYDeg(1))+o.targetHeightDeg*0.75;
    o.minScreenWidthDeg=1+o.targetHeightDeg*2;
    o.maxViewingDistanceCm=round(0.1*cal.screenWidthMm/(2*tand(o.minScreenWidthDeg/2)));
