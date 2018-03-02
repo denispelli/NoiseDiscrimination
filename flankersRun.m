91e32ddf9a37420c32b16c5163b0a5d6898ec46a
denispelli <denis.pelli@nyu.edu>
2018-03-02 15:37:36 -0500

    minor

--- flankersRun.m
+++ flankersRun.m
@@ -28,7 +28,7 @@ if false && ~streq(cal.macModelName,'MacBookPro14,3')
    warning('PRETENDING THIS IS A 15" MacBook Pro 2017');
 end
 
-o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
+% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 o.useDynamicNoiseMovie=true;
 if true
    o.useFlankers=true;
