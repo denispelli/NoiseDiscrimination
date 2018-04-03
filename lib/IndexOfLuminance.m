7b3d9107cdec6549ae75a616689be5056b7f7f5c
denispelli <denis.pelli@nyu.edu>
2018-04-02 20:47:07 -0400

    Enhance error message for out-of-range.

--- lib/IndexOfLuminance.m
+++ lib/IndexOfLuminance.m
@@ -8,10 +8,11 @@ image=cal.nFirst+(cal.nLast-cal.nFirst)*(imageLuminance-cal.LFirst)/(cal.LLast-c
 image=round(image);
 ii=round(image(:))<cal.nFirst | round(image(:))>cal.nLast;
 if any(ii)
+    msg0=sprintf('Contrast too high? Luminances (mean %.1f) ranging %.1f to %.1f exceed linearized range %.1f to %.1f cd/m^2. ',mean(imageLuminance(:)),min(imageLuminance(:)),max(imageLuminance(:)),cal.LFirst,cal.LLast);
     msg1=sprintf('%.0f out-of-range pixels, with values [',sum(ii));
     msg2=sprintf(' %.0f',unique(image(ii)));
     msg3=sprintf('], were bounded to the range %d to %d.',cal.nFirst,cal.nLast);
-    warning('%s%s%s',msg1,msg2,msg3);
+    warning('%s%s%s',msg0,msg1,msg2,msg3);
     image=min(image,cal.nLast);
     image=max(image,cal.nFirst);
 end
