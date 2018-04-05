83907a5da0b684b750d2e3b1dbd28b6198d31229
denispelli <denis.pelli@nyu.edu>
2018-04-05 06:23:11 -0400

    indent 4 spaces

--- EvsNRun.m
+++ EvsNRun.m
@@ -1,7 +1,7 @@
 % EvsNRun.m
 % Measure E vs N for each of 3 conditions, with and
 % without noise. We expect linearity always.
-% January 31, 2018
+% April 5, 2018
 % Denis Pelli
 
 % STANDARD CONDITION
@@ -18,7 +18,7 @@ clear o oo
 skipDataCollection=false; % Enable skipDataCollection to check plotting before we have data.
 o.questPlusEnable=false;
 if verLessThan('matlab','R2013b')
-   error('This MATLAB is too old. We need MATLAB 2013b or better to use the function "struct2table".');
+    error('This MATLAB is too old. We need MATLAB 2013b or better to use the function "struct2table".');
 end
 if o.questPlusEnable && ~exist('qpInitialize','file')
     error('This script requires the QuestPlus package. Please get it from https://github.com/BrainardLab/mQUESTPlus.')
@@ -28,11 +28,11 @@ cal=OurScreenCalibrations(0);
 o.seed=[]; % Fresh.
 % o.seed=uint32(1506476580); % Copy seed value here to reproduce an old table of conditions.
 if isempty(o.seed)
-   rng('shuffle'); % Use clock to seed the random number generator.
-   generator=rng;
-   o.seed=generator.Seed;
+    rng('shuffle'); % Use clock to seed the random number generator.
+    generator=rng;
+    o.seed=generator.Seed;
 else
-   rng(o.seed);
+    rng(o.seed);
 end
 % o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 
@@ -53,83 +53,83 @@ o.moviePostSec=0.2;
 o.targetMarkDeg=1;
 o.fixationCrossDeg=3;
 if false
-% Use QuestPlus to measure steepness.
- o.questPlusEnable=true;
- o.questPlusSteepnesses=1:0.1:5;
- o.questPlusGuessingRates=1/o.alternatives;
- o.questPlusLapseRates=0:0.01:0.05;
- o.questPlusLogContrasts=-2.5:0.05:0.5;
- o.questPlusPrint=true;
- o.questPlusPlot=true;
+    % Use QuestPlus to measure steepness.
+    o.questPlusEnable=true;
+    o.questPlusSteepnesses=1:0.1:5;
+    o.questPlusGuessingRates=1/o.alternatives;
+    o.questPlusLapseRates=0:0.01:0.05;
+    o.questPlusLogContrasts=-2.5:0.05:0.5;
+    o.questPlusPrint=true;
+    o.questPlusPlot=true;
 end
 
 %% SAVE CONDITIONS IN oo
 oo={};
 for domain=1:3
-   switch domain
-      case 1
-         % photon
-         o.conditionName='photon';
-         o.eccentricityXYDeg=[0 0];
-         o.targetCyclesPerDeg=4;
-         o.targetDurationSec=0.1;
+    switch domain
+        case 1
+            % photon
+            o.conditionName='photon';
+            o.eccentricityXYDeg=[0 0];
+            o.targetCyclesPerDeg=4;
+            o.targetDurationSec=0.1;
             o.desiredLuminance=2.5; % cd/m^2
             o.desiredLuminanceFactor=[];
             o.useFilter=true;
             o.fixationCrossWeightDeg=0.1; % Typically 0.03. Make it much thicker for scotopic testing.
-      case 2
-         % cortical
-         o.conditionName='cortical';
-         o.eccentricityXYDeg=[0 0];
-         o.targetCyclesPerDeg=0.5;
-         o.targetDurationSec=0.4;
-		o.desiredLuminance=[];
-		o.desiredLuminanceFactor=1;
-		o.useFilter=false;
-		o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
-      case 3
-         % ganglion
-         o.conditionName='ganglion';
-         o.eccentricityXYDeg=[30 0];
-            o.nearPointXYInUnitSquare=[0.80 0.5];
-         o.targetCyclesPerDeg=0.5;
-         o.targetDurationSec=0.2;
+        case 2
+            % cortical
+            o.conditionName='cortical';
+            o.eccentricityXYDeg=[0 0];
+            o.targetCyclesPerDeg=0.5;
+            o.targetDurationSec=0.4;
             o.desiredLuminance=[];
             o.desiredLuminanceFactor=1;
             o.useFilter=false;
             o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
-   end
-   o.eyes='right'; % 'left', 'right', 'both'.
-      o.blankingRadiusReEccentricity=0; % No blanking.
-      if 0
-         % Target letter
-         o.targetKind='letter';
-         o.font='Sloan';
-         o.alphabet='DHKNORSVZ';
-      else
-         % Target gabor
-         o.targetKind='gabor';
-         o.targetGaborOrientationsDeg=[0 45 90 135];
-         o.targetGaborNames='1234';
-         o.alphabet=o.targetGaborNames;
-      end
-      o.alternatives=length(o.alphabet);
-      o.useDynamicNoiseMovie=true;
-      if all(o.eccentricityXYDeg==0)
-         o.markTargetLocation=false;
+        case 3
+            % ganglion
+            o.conditionName='ganglion';
+            o.eccentricityXYDeg=[30 0];
+            o.nearPointXYInUnitSquare=[0.80 0.5];
+            o.targetCyclesPerDeg=0.5;
+            o.targetDurationSec=0.2;
+            o.desiredLuminance=[];
+            o.desiredLuminanceFactor=1;
+            o.useFilter=false;
+            o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
+    end
+    o.eyes='right'; % 'left', 'right', 'both'.
+    o.blankingRadiusReEccentricity=0; % No blanking.
+    if 0
+        % Target letter
+        o.targetKind='letter';
+        o.font='Sloan';
+        o.alphabet='DHKNORSVZ';
+    else
+        % Target gabor
+        o.targetKind='gabor';
+        o.targetGaborOrientationsDeg=[0 45 90 135];
+        o.targetGaborNames='1234';
+        o.alphabet=o.targetGaborNames;
+    end
+    o.alternatives=length(o.alphabet);
+    o.useDynamicNoiseMovie=true;
+    if all(o.eccentricityXYDeg==0)
+        o.markTargetLocation=false;
         o.blankingRadiusReTargetHeight=2;
         o.fixationCrossDeg=10;
-      else
-         o.markTargetLocation=true;
+    else
+        o.markTargetLocation=true;
         o.blankingRadiusReTargetHeight=0;
         o.fixationCrossDeg=3;
-      end
-   for noiseSD=Shuffle([0 2^-2 2^-1.5 2^-1 2^-0.5 2^0]*0.16)
-      o.targetHeightDeg=o.targetGaborCycles/o.targetCyclesPerDeg;
-      o.noiseCheckDeg=o.targetHeightDeg/20;
-      o.noiseSD=noiseSD;
-            oo{end+1}=o;
-   end
+    end
+    for noiseSD=Shuffle([0 2^-2 2^-1.5 2^-1 2^-0.5 2^0]*0.16)
+        o.targetHeightDeg=o.targetGaborCycles/o.targetCyclesPerDeg;
+        o.noiseCheckDeg=o.targetHeightDeg/20;
+        o.noiseSD=noiseSD;
+        oo{end+1}=o;
+    end
 end
 for i=1:length(oo)
     oo{i}.condition=i; % Number the conditions
@@ -150,13 +150,13 @@ disp(tt) % Print list of conditions.
 
 %% RUN THE CONDITIONS
 if ~skipDataCollection
-   % Typically, you'll select just a few of the conditions stored in oo
+    % Typically, you'll select just a few of the conditions stored in oo
     % that you want to run now. Select them from the printout of "tt" in your
     % Command Window.
-   % NOTE: Typically, conditions with the same conditionName are randonly shuffled
-   % every time you run this, unless you set o.seed, above, to the 'seed'
-   % used to generate the table you want to reproduce.
-   clear oOut
+    % NOTE: Typically, conditions with the same conditionName are randonly shuffled
+    % every time you run this, unless you set o.seed, above, to the 'seed'
+    % used to generate the table you want to reproduce.
+    clear oOut
     for oi=1:length(oo) % Edit this line to select which conditions to run now.
         o=oo{oi};
         o.runNumber=oi;
@@ -164,7 +164,7 @@ if ~skipDataCollection
         if exist('oOut','var')
             % Reuse answers from immediately preceding run.
             o.experimenter=oOut.experimenter;
-         o.observer=oOut.observer;
+            o.observer=oOut.observer;
             % Setting o.useFilter false forces o.filterTransmission=1.
             o.filterTransmission=oOut.filterTransmission;
         end
@@ -172,38 +172,38 @@ if ~skipDataCollection
         oo{oi}=oOut; % Save results in oo.
         if oOut.quitSession
             break
-      end
+        end
         fprintf('\n');
     end % for oi=1:length(oo)
 end % if ~skipDataCollection
 
- %% PRINT SUMMARY OF RESULTS
-   % All these vars must be defined in every condition.
+%% PRINT SUMMARY OF RESULTS
+% All these vars must be defined in every condition.
 vars={'condition' 'experiment' 'conditionName' ...
     'useFilter' 'eccentricityXYDeg' ...
     'targetDurationSec' 'targetCyclesPerDeg' ...
     'targetHeightDeg' 'targetGaborCycles' 'noiseSD' };
-   tt=table;
-   for i=1:length(oo)
-       t=struct2table(oo{i},'AsArray',true);
-       if ~all(ismember({'trials' 'contrast' 'transcript'},t.Properties.VariableNames)) || isempty(t.trials) || t.trials==0
-           % Skip condition without data.
-           continue
-       end
-       % Warn, skip the condition, and report which fields were missing.
-       ok=ismember(vars,t.Properties.VariableNames);
-       if ~all(ok)
-           missing=join(vars(~ok),' ');
-           warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
-           continue
-       end
-       tt(i,:)=t(1,vars);
-   end
-   disp(tt) % Print summary.
+tt=table;
+for i=1:length(oo)
+    t=struct2table(oo{i},'AsArray',true);
+    if ~all(ismember({'trials' 'contrast' 'transcript'},t.Properties.VariableNames)) || isempty(t.trials) || t.trials==0
+        % Skip condition without data.
+        continue
+    end
+    % Warn, skip the condition, and report which fields were missing.
+    ok=ismember(vars,t.Properties.VariableNames);
+    if ~all(ok)
+        missing=join(vars(~ok),' ');
+        warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
+        continue
+    end
+    tt(i,:)=t(1,vars);
+end
+disp(tt) % Print summary.
 
-   %% SAVE SUMMARY OF RESULTS
-   o=oOut;
-   o.summaryFilename=[o.dataFilename '.summary' ];
-   writetable(tt,fullfile(o.dataFolder,[o.summaryFilename '.csv']));
-   save(fullfile(o.dataFolder,[o.summaryFilename '.mat']),'tt','oo');
-   fprintf('Summary saved in data folder as "%s" with extensions ".csv" and ".mat".\n',o.summaryFilename);
+%% SAVE SUMMARY OF RESULTS
+o=oOut;
+o.summaryFilename=[o.dataFilename '.summary' ];
+writetable(tt,fullfile(o.dataFolder,[o.summaryFilename '.csv']));
+save(fullfile(o.dataFolder,[o.summaryFilename '.mat']),'tt','oo');
+fprintf('Summary saved in data folder as "%s" with extensions ".csv" and ".mat".\n',o.summaryFilename);
