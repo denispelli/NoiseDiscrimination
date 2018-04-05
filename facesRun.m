b80e387a198fd8dad7c32807c8933787b6289d18
denispelli <denis.pelli@nyu.edu>
2018-04-05 06:20:00 -0400

    Shuffle conditions, clean up

--- facesRun.m
+++ facesRun.m
@@ -6,23 +6,14 @@
 clear o oo
 skipDataCollection=false; % Enable skipDataCollection to check plotting before we have data.
 o.questPlusEnable=false;
-if verLessThan('matlab','R2017b')
-   error('This MATLAB is too old. We need MATLAB 2017b or better to use the function "rgb2lin".');
+if ~exist('rgb2lin','file')
+   error('This MATLAB %s is too old. We need MATLAB 2017b or better to use the function "rgb2lin".',version('-release'));
 end
 if o.questPlusEnable && ~exist('qpInitialize','file')
    error('This script requires the QuestPlus package. Please get it from https://github.com/BrainardLab/mQUESTPlus.')
 end
-addpath(fullfile(fileparts(mfilename('fullpath')),'lib')); % folder in same directory as this M file
+addpath(fullfile(fileparts(mfilename('fullpath')),'lib')); % Folder in same directory as this M file.
 cal=OurScreenCalibrations(0);
-if false && ~streq(cal.macModelName,'MacBookPro14,3')
-   % For debugging, if this isn't a 15" MacBook Pro 2017, pretend it is.
-   cal.screenWidthMm=330; % 13"
-   cal.screenHeightMm=206; % 8.1"
-   warning('PRETENDING THIS IS A 15" MacBook Pro 2017');
-end
-
-%% CREATE LIST OF CONDITIONS TO BE TESTED
-% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 o.seed=[]; % Fresh.
 % o.seed=uint32(1506476580); % Copy seed value here to reproduce an old table of conditions.
 if isempty(o.seed)
@@ -32,6 +23,9 @@ if isempty(o.seed)
 else
    rng(o.seed);
 end
+% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
+
+%% CREATE LIST OF CONDITIONS TO BE TESTED
 o.symmetricLuminanceRange=false; % Allow maximum brightness.
 o.desiredLuminanceFactor=2; % Maximize brightness.
 if false
@@ -66,113 +60,109 @@ o.observer='';
 o.noiseSD=0;
 o.thresholdParameter='contrast';
 o.conditionName='threshold';
-o.condition=1;
-for beautyTask=0:1
+if false
+% Use QuestPlus to measure steepness.
+ o.questPlusEnable=true;
+ o.questPlusSteepnesses=1:0.1:5;
+ o.questPlusGuessingRates=1/o.alternatives;
+ o.questPlusLapseRates=0:0.01:0.05;
+ o.questPlusLogContrasts=-2.5:0.05:0.5;
+ o.questPlusPrint=true;
+ o.questPlusPlot=true;
+end
+if all(o.eccentricityXYDeg==0)
+ o.markTargetLocation=false;
+else
+ o.markTargetLocation=true;
+end
+o.blankingRadiusReTargetHeight=0;
+o.targetMarkDeg=1;
+o.fixationCrossDeg=3;
+        o.alternatives=length(o.alphabet);
+
+%% SAVE CONDITIONS IN oo
+oo={};
+for beautyTask=Shuffle(0:1)
    if beautyTask
       o.task='rate';
    else
       o.task='identify';
    end
-   for duration=[0.2 1]
+   for duration=Shuffle([0.2 1])
        o.targetDurationSec=duration;
-       if ~exist('oo','var')
-           oo=o;
-       else
-           oo(o.condition)=o;
-       end
-       o.condition=1+o.condition;
+            oo{end+1}=o;
    end
 end
+for i=1:length(oo)
+    oo{i}.condition=i; % Number the conditions
+end
 
-%% PRINT THE TABLE, ONE CONDITION PER ROW
-t=struct2table(oo,'AsArray',true);
-% We list parameters here in the order that we want them to appear as
-% columns in the table, which we print in the Command Window. 
+%% PRINT THE LIST OF CONDITIONS (ONE PER ROW)
+% All these vars must be defined in every condition.
 vars={'seed' 'condition' 'task' 'targetDurationSec' 'targetHeightDeg' 'noiseSD'};
-disp(t(:,vars)) % Print the oo list of conditions.
-fprintf('To recreate this table, set o.seed (in line 27) to the value of "seed" listed in the above table.\n');
+tt=table;
+for i=1:length(oo)
+    t=struct2table(oo{i},'AsArray',true);
+    tt(i,:)=t(1,vars);
+end
+disp(tt) % Print list of conditions.
 
 %% RUN THE CONDITIONS
-if ~skipDataCollection && true
+if ~skipDataCollection
    % Typically, you'll select just a few of the conditions stored in oo
-   % that you want to run now. Select them from the printout of "t" in your
+    % that you want to run now. Select them from the printout of "tt" in your
    % Command Window.
-   % NOTE: Conditions with the same conditionName are randonly shuffled
+   % NOTE: Typically, conditions with the same conditionName are randonly shuffled
    % every time you run this, unless you set o.seed, above, to the 'seed'
    % used to generate the table you want to reproduce.
    clear oOut
    for oi=1:length(oo) % Edit this line to select which conditions to run now.
-      o=oo(oi);
+        o=oo{oi};
+        o.runNumber=oi;
+        o.runsDesired=length(oo);
       if exist('oOut','var')
          % Reuse answers from immediately preceding run.
          o.experimenter=oOut.experimenter;
          o.observer=oOut.observer;
       end
-      o.alternatives=length(o.alphabet);
-      if all(o.eccentricityXYDeg==0)
-         o.markTargetLocation=false;
-      else
-         o.markTargetLocation=true;
-      end
-      o.blankingRadiusReTargetHeight=0;
-      o.targetMarkDeg=1;
-      o.fixationCrossDeg=3;
-      if o.questPlusEnable
-         % Use QuestPlus when we don't know all the parameters.
-         o.questPlusSteepnesses=1:0.1:5;
-         o.questPlusGuessingRates=1/o.alternatives:0.02:0.5;
-         o.questPlusLapseRates=0:0.01:0.05;
-         o.questPlusLogContrasts=-2.5:0.05:0.5;
-         o.questPlusPrint=true;
-         o.questPlusPlot=true;
-      end
-      oOut=NoiseDiscrimination(o);
-      oo(oi).trials=oOut.trials; % Always defined.
-      if isfield(oOut,'psych')
-         fprintf(['<strong>%s: noiseSD %.2f, log N %.2f, flankerSpacingDeg %.1f, '...
-            'target contrast %.3f, threshold flankerContrast %.3f</strong>\n'],...
-            oOut.conditionName,oOut.noiseSD,log10(oOut.N),oOut.flankerSpacingDeg,...
-            oOut.contrast,oOut.flankerContrast);
-         oo(oi).experimenter=oOut.experimenter;
-         oo(oi).observer=oOut.observer;
-         oo(oi).contrast=oOut.contrast;
-         oo(oi).E1=oOut.E1;
-         oo(oi).alternatives=oOut.alternatives;
-         oo(oi).targetKind=oOut.targetKind;
-         oo(oi).LBackground=oOut.LBackground;
-         oo(oi).targetDurationSec=oOut.targetDurationSec;
-         oo(oi).eccentricityXYDeg=oOut.eccentricityXYDeg;
-         oo(oi).targetCyclesPerDeg=oOut.targetCyclesPerDeg;
-         oo(oi).data=oOut.data;
-         oo(oi).psych=oOut.psych;
-         oo(oi).guess=oOut.guess;
-         oo(oi).lapse=oOut.lapse;
-         oo(oi).steepness=oOut.steepness;
-         oo(oi).dataFilename=oOut.dataFilename;
-         oo(oi).dataFolder=oOut.dataFolder;
-      end
+        oOut=NoiseDiscrimination(o); % RUN THE EXPERIMENT!
+        oo{oi}=oOut; % Save results in oo.
       if oOut.quitSession
          break
       end
+        fprintf('\n');
    end
+for i=1:length(oo)
+    oo{i}.condition=i; % Number the conditions
+end
    
    %% PRINT SUMMARY OF RESULTS
-   t=struct2table(oo(1:oi),'AsArray',true);
-   rows=t.trials>0;
-   if ~rows
-      return
-   end
+   % All these vars must be defined in every condition.
    vars={'condition' 'observer' 'trials'  'task' 'targetDurationSec' 'targetHeightDeg' 'contrast' 'guess' 'lapse' 'steepness' 'seed' };
-   if any(rows)
-      disp(t(rows,vars)) % Print the oo list of conditions, with measured flanker threshold.
+   tt=table;
+   for i=1:length(oo)
+       t=struct2table(oo{i},'AsArray',true);
+       if ~all(ismember({'trials' 'contrast' 'transcript'},t.Properties.VariableNames)) || isempty(t.trials) || t.trials==0
+           % Skip condition without data.
+           continue
+       end
+       % Warn, skip the condition, and report which fields were missing.
+       ok=ismember(vars,t.Properties.VariableNames);
+       if ~all(ok)
+           missing=join(vars(~ok),' ');
+           warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
+           continue
+       end
+       tt(i,:)=t(1,vars);
    end
+   disp(tt) % Print summary.
    
    %% SAVE SUMMARY OF RESULTS
    o=oOut;
    o.summaryFilename=[o.dataFilename '.summary' ];
-   writetable(t,fullfile(o.dataFolder,[o.summaryFilename '.csv']));
-   save(fullfile(o.dataFolder,[o.summaryFilename '.mat']),'t','rows','vars','oo');
-   fprintf('Summary saved as "%s" with extensions ".csv" and ".mat".\n',o.summaryFilename);
+   writetable(tt,fullfile(o.dataFolder,[o.summaryFilename '.csv']));
+   save(fullfile(o.dataFolder,[o.summaryFilename '.mat']),'tt','oo');
+   fprintf('Summary saved in data folder as "%s" with extensions ".csv" and ".mat".\n',o.summaryFilename);
 
    %% PLOT IT
    tBeauty=t(streq(t.task,'rate'),{'targetDurationSec' 'contrast'});
