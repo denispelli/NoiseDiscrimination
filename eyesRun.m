b47a9bcf1bd3022fb435e750125c1b10ef00aef8
denispelli <denis.pelli@nyu.edu>
2018-04-05 06:21:27 -0400

    clean up, indent 4 spaces

--- eyesRun.m
+++ eyesRun.m
@@ -23,221 +23,204 @@ if verLessThan('matlab','R2013b')
     error('This MATLAB is too old. We need MATLAB 2013b or better to use the function "struct2table".');
 end
 if o.questPlusEnable && ~exist('qpInitialize','file')
-   error('This script requires the QuestPlus package. Please get it from https://github.com/BrainardLab/mQUESTPlus.')
+    error('This script requires the QuestPlus package. Please get it from https://github.com/BrainardLab/mQUESTPlus.')
 end
-addpath(fullfile(fileparts(mfilename('fullpath')),'lib')); % folder in same directory as this M file
+addpath(fullfile(fileparts(mfilename('fullpath')),'lib')); % Folder in same directory as this M file.
 cal=OurScreenCalibrations(0);
 if false && ~streq(cal.macModelName,'MacBookPro14,3')
-   % For debugging, if this isn't a 15" MacBook Pro 2017, pretend it is.
-   cal.screenWidthMm=330; % 13"
-   cal.screenHeightMm=206; % 8.1"
-   warning('PRETENDING THIS IS A 15" MacBook Pro 2017');
+    % For debugging, if this isn't a 15" MacBook Pro 2017, pretend it is.
+    cal.screenWidthMm=330; % 13"
+    cal.screenHeightMm=206; % 8.1"
+    warning('PRETENDING THIS IS A 15" MacBook Pro 2017');
 end
+o.seed=[]; % Fresh.
+% o.seed=uint32(1506476580); % Copy seed value here to reproduce an old table of conditions.
+% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 
 %% CREATE LIST OF CONDITIONS TO BE TESTED
-% THREE DOMAINS
 % In each of the 3 domains: photon, cortical, ganglion
 % Two noise levels, noiseSD: 0 0.16
-%
-% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
-o.responseScreenAbsoluteContrast=1;
 o.experiment='eyes';
-o.condition=1;
+o.thresholdParameter='contrast';
 o.viewingDistanceCm=40;
 o.eyes='right';
+o.trialsPerRun=40;
 o.targetGaborCycles=3; % cycles of the sinewave in targetHeight
-for domain=3
-   switch domain
-      case 1
-         % photon
-         o.conditionName='photon';
-         o.eccentricityXYDeg=[0 0];
-         o.targetCyclesPerDeg=4;
-         o.targetDurationSec=0.1;
-         o.desiredLuminance=2.5; % cd/m^2
-         o.desiredLuminanceFactor=[];
-         o.useFilter=true;
-         o.fixationCrossWeightDeg=0.1; % Typically 0.03. Make it much thicker for scotopic testing.
-         % o.minScreenWidthDeg=30; % Big to determine pupil size.
-      case 2
-         % cortical
-         o.conditionName='cortical';
-         o.eccentricityXYDeg=[0 0];
-         o.targetCyclesPerDeg=0.5;
-         o.targetDurationSec=0.4;
-         o.desiredLuminance=[];
-         o.desiredLuminanceFactor=1;
-         o.useFilter=false;
-         o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
-         %  o.minScreenWidthDeg=10;
-      case 3
-         % ganglion
-         o.conditionName='ganglion';
-         o.eccentricityXYDeg=[30 0];
-         o.nearPointXYInUnitSquare=[0.80 0.5];
-         o.targetCyclesPerDeg=0.5;
-         o.targetDurationSec=0.2;
-         o.desiredLuminance=[];
-         o.desiredLuminanceFactor=1;
-         o.useFilter=false;
-         o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
-         % o.minScreenWidthDeg=10;
-   end
-   for eyes=Shuffle({'right' 'both'})
-      %    for eyes={'right'}
-      o.eyes=eyes{1};
-      for noiseSD=Shuffle([0 0.3])
-         %       for noiseSD=0.3
-         o.noiseType='binary';
-         o.targetHeightDeg=o.targetGaborCycles/o.targetCyclesPerDeg;
-         %          o.minScreenWidthDeg=1+abs(o.eccentricityXYDeg(1))+o.targetHeightDeg*0.75;
-         o.minScreenWidthDeg=1+o.targetHeightDeg*2;
-         o.maxViewingDistanceCm=round(0.1*cal.screenWidthMm/(2*tand(o.minScreenWidthDeg/2)));
-         o.viewingDistanceCm=min([o.maxViewingDistanceCm 40]);
-         o.noiseCheckDeg=o.targetHeightDeg/10;
-         o.noiseSD=noiseSD;
-         if ~exist('oo','var')
-            oo=o;
-         else
-            oo(end+1)=o;
-         end
-      end
-   end
+o.blankingRadiusReEccentricity=0; % No blanking.
+o.moviePreSec=0.3;
+o.moviePostSec=0.3;
+o.targetMarkDeg=1;
+o.fixationCrossDeg=3;
+if false
+    % Target letter
+    o.targetKind='letter';
+    o.font='Sloan';
+    o.alphabet='DHKNORSVZ';
+    o.contrast=-1; % negative contrast.
+else
+    % Target gabor
+    o.targetKind='gabor';
+    o.targetGaborOrientationsDeg=[0 45 90 135];
+    o.targetGaborNames='1234';
+    o.alphabet=o.targetGaborNames;
+    o.contrast=1; % positive contrast.
 end
+if false
+    % Use QuestPlus to measure steepness.
+    o.questPlusEnable=true;
+    o.questPlusSteepnesses=1:0.1:5;
+    o.questPlusGuessingRates=1/o.alternatives;
+    o.questPlusLapseRates=0:0.01:0.05;
+    o.questPlusLogContrasts=-2.5:0.05:0.5;
+    o.questPlusPrint=true;
+    o.questPlusPlot=true;
+end
+% o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
 
-%% Number the conditions, and print the list.
+%% SAVE CONDITIONS IN oo
+oo={};
+for domain=1:3
+    switch domain
+        case 1
+            % photon
+            o.conditionName='photon';
+            o.eccentricityXYDeg=[0 0];
+            o.targetCyclesPerDeg=4;
+            o.targetDurationSec=0.1;
+            o.desiredLuminance=2.5; % cd/m^2
+            o.desiredLuminanceFactor=[];
+            o.useFilter=true;
+            o.fixationCrossWeightDeg=0.1; % Typically 0.03. Make it much thicker for scotopic testing.
+        case 2
+            % cortical
+            o.conditionName='cortical';
+            o.eccentricityXYDeg=[0 0];
+            o.targetCyclesPerDeg=0.5;
+            o.targetDurationSec=0.4;
+            o.desiredLuminance=[];
+            o.desiredLuminanceFactor=1;
+            o.useFilter=false;
+            o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
+        case 3
+            % ganglion
+            o.conditionName='ganglion';
+            o.eccentricityXYDeg=[30 0];
+            o.nearPointXYInUnitSquare=[0.80 0.5];
+            o.targetCyclesPerDeg=0.5;
+            o.targetDurationSec=0.2;
+            o.desiredLuminance=[];
+            o.desiredLuminanceFactor=1;
+            o.useFilter=false;
+            o.fixationCrossWeightDeg=0.03; % Typically 0.03. Make it much thicker for scotopic testing.
+    end
+    o.alternatives=length(o.alphabet);
+    o.useDynamicNoiseMovie=true;
+    if all(o.eccentricityXYDeg==0)
+        o.markTargetLocation=false;
+        o.blankingRadiusReTargetHeight=2;
+        o.fixationCrossDeg=10;
+    else
+        o.markTargetLocation=true;
+        o.blankingRadiusReTargetHeight=0;
+        o.fixationCrossDeg=3;
+    end
+    for eyes=Shuffle({'right' 'both'})
+        %    for eyes={'right'}
+        o.eyes=eyes{1};
+        for noiseSD=Shuffle([0 0.3])
+            %       for noiseSD=0.3
+            o.noiseType='binary';
+            o.targetHeightDeg=o.targetGaborCycles/o.targetCyclesPerDeg;
+            o.noiseCheckDeg=o.targetHeightDeg/10;
+            o.noiseSD=noiseSD;
+            oo{end+1}=o;
+        end % for noiseSD
+    end % for eyes=
+end % for domain=1:3
 for i=1:length(oo)
-   oo(i).condition=i;
+    oo{i}.condition=i; % Number the conditions
 end
-t=struct2table(oo,'AsArray',true);
-vars={'condition' 'experiment' 'conditionName' ...
-   'useFilter' 'eccentricityXYDeg' ...
-   'targetDurationSec' 'targetCyclesPerDeg' ...
-   'targetGaborCycles' 'targetHeightDeg' 'noiseSD' };
-t(:,vars) % Print the oo list of conditions.
 
-if skipDataCollection
-   % NOT IMPLEMENTED.
-   % FAKE SOME DATA TO CHECK THE ANALYSIS & PLOTTING.
-   data=table2struct(t);
-   for i=1:length(data)
-      data(i).E=10*data(i).noiseSD+1e-5*(1+floor((i-1)/8));
-      data(i).trialsPerRun=40;
-      data(i).N=data(i).noiseSD;
-      data(i).experimenter='Experimenter';
-      data(i).observer='Observer';
-      data(i).targetKind='gabor';
-      data(i).noiseType='gaussian';
-      data(i).LBackground=280*data(i).luminanceFactor;
-   end
-   steepnessAnalyze(data);
+%% PRINT THE LIST OF CONDITIONS (ONE PER ROW)
+% All these vars must be defined in every condition.
+vars={'condition' 'experiment' 'conditionName' ...
+    'useFilter' 'eccentricityXYDeg' ...
+    'targetDurationSec' 'targetCyclesPerDeg' ...
+    'targetHeightDeg' 'targetGaborCycles' 'noiseSD' };
+tt=table;
+for i=1:length(oo)
+    t=struct2table(oo{i},'AsArray',true);
+    tt(i,:)=t(1,vars);
 end
-if ~skipDataCollection && true
-   %% RUN THE CONDITIONS
-   % Typically, you'll select just a few of the conditions stored in oo
-   % that you want to run now. Select them from the printout of "t" above.
-   clear oOut
-   for oi=1:length(oo) % Edit this line to select the conditions you want to run now.
-      o=oo(oi);
-      % o.useFractionOfScreen=0.4; % 0: normal, 0.5: small for debugging.
-      o.trialsPerRun=40;
-      if exist('oOut','var')
-         % Copy answers from immediately preceding run.
-         o.experimenter=oOut.experimenter;
-         o.observer=oOut.observer;
-         % Setting o.useFilter false forces o.filterTransmission=1.
-         o.filterTransmission=oOut.filterTransmission;
-      end
-      o.blankingRadiusReEccentricity=0; % No blanking.
-      if false
-         % Target letter
-         o.targetKind='letter';
-         o.font='Sloan';
-         o.alphabet='DHKNORSVZ';
-         o.contrast=-1; % negative contrast.
-      else
-         % Target gabor
-         o.targetKind='gabor';
-         o.targetGaborOrientationsDeg=[0 45 90 135];
-         o.targetGaborNames='1234';
-         o.alphabet=o.targetGaborNames;
-         o.contrast=1; % positive contrast.
-      end
-      o.alternatives=length(o.alphabet);
-      o.useDynamicNoiseMovie=true;
-      if all(o.eccentricityXYDeg==0)
-         o.markTargetLocation=false;
-         o.blankingRadiusReTargetHeight=2;
-         o.fixationCrossDeg=10;
-      else
-         o.markTargetLocation=true;
-         o.blankingRadiusReTargetHeight=0;
-         o.fixationCrossDeg=3;
-      end
-      o.moviePreSec=0.3;
-      o.moviePostSec=0.3;
-      o.targetMarkDeg=1;
-      if 0
-         % Use QuestPlus to measure steepness.
-         o.questPlusEnable=true;
-         o.questPlusSteepnesses=1:0.1:5;
-         o.questPlusGuessingRates=1/o.alternatives;
-         o.questPlusLapseRates=0:0.01:0.05;
-         o.questPlusLogContrasts=-2.5:0.05:0.5;
-         o.questPlusPrint=true;
-         o.questPlusPlot=true;
-      end
-      oOut=NoiseDiscrimination(o);
-      oo(oi).trials=oOut.trials;
-      if isempty(oo(oi).trials)
-         oo(oi).trials=0;
-      end
-      %       fprintf(['%s: %.1f cd/m^2, luminanceFactor %.2f, filterTransmission %.3f\n'],...
-      %          o.conditionName,oOut.luminanceAtEye,oOut.luminanceFactor,oOut.filterTransmission);
-      if isfield(oOut,'psych')
-         fprintf(['<strong>%s: luminanceAtEye %.1f cd/m^2, '...
-            'eccentricity %.1f %.1f deg, '...
-            'duration %.2f s, cyclesPerDeg %.1f, '...
-            'noiseSD %.2f, log N %.2f, '...
-            'contrast %.3f</strong>\n'],...
-            oOut.conditionName,oOut.luminanceAtEye,...
-            oOut.eccentricityXYDeg, ...
-            oOut.targetDurationSec, oOut.targetCyclesPerDeg,...
-            oOut.noiseSD,log10(oOut.N),...
-            oOut.contrast);
-         oo(oi).experimenter=oOut.experimenter;
-         oo(oi).observer=oOut.observer;
-         oo(oi).filterTransmission=oOut.filterTransmission;
-         oo(oi).luminanceAtEye=oOut.luminanceAtEye;
-         oo(oi).contrast=oOut.contrast;
-         oo(oi).N=oOut.N;
-         oo(oi).data=oOut.data;
-         oo(oi).psych=oOut.psych;
-      end
-      if oOut.quitSession
-         break
-      end
-   end
-   
-   %% PRINT THE RESULTS
-   t=struct2table(oo(1:oi),'AsArray',true);
-   if iscell(t.trials)
-      % This case may never occur. Just in case.
-      rows=false(size(t.trials));
-      for i=1:length(t.trials)
-         trials=t.trials{i};
-         rows(i)=~isempty(trials) && trials>0;
-      end
-   else
-      rows=t.trials>0;
-   end
-   %    vars={'condition' 'experiment' 'conditionName' ...
-   %       'viewingDistanceCm' 'eyes'  ...
-   %       'luminanceAtEye' 'eccentricityXYDeg' ...
-   %       'targetDurationSec' 'targetCyclesPerDeg' ...
-   %       'targetGaborCycles' 'targetHeightDeg' 'noiseSD' 'noiseCheckDeg'};
-   vars={'experiment' 'condition' 'observer' 'trials' 'luminanceAtEye' 'eccentricityXYDeg' 'targetDurationSec' 'targetCyclesPerDeg' 'noiseSD' 'N' 'contrast'};
-   if any(rows)
-      disp(t(rows,vars)) % Print the oo list of conditions, with measured threshold.
-   end
-end % Run the selected conditions
+disp(tt) % Print list of conditions.
+
+%% RUN THE CONDITIONS
+if ~skipDataCollection
+    % Typically, you'll select just a few of the conditions stored in oo
+    % that you want to run now. Select them from the printout of "tt" in your
+    % Command Window.
+    clear oOut
+    for oi=1:length(oo) % Edit this line to select which conditions to run now.
+        o=oo{oi};
+        o.runNumber=oi;
+        o.runsDesired=length(oo);
+        o.alternatives=length(o.alphabet);
+        if exist('oOut','var')
+            % Reuse answers from immediately preceding run.
+            o.experimenter=oOut.experimenter;
+            o.observer=oOut.observer;
+            % Setting o.useFilter false forces o.filterTransmission=1.
+            o.filterTransmission=oOut.filterTransmission;
+        end
+        oOut=NoiseDiscrimination(o); % RUN THE EXPERIMENT!
+        oo{oi}=oOut; % Save results in oo.
+        if oOut.quitSession
+            break
+        end
+        fprintf('\n');
+    end % for oi=1:length(oo)
+end % if ~skipDataCollection
+
+%% PRINT THE LIST OF CONDITIONS (ONE PER ROW)
+% All these vars must be defined in every condition.
+vars={'condition' 'experiment' 'conditionName' ...
+    'useFilter' 'eccentricityXYDeg' ...
+    'targetDurationSec' 'targetCyclesPerDeg' ...
+    'targetHeightDeg' 'targetGaborCycles' 'noiseSD' };
+tt=table;
+for i=1:length(oo)
+    t=struct2table(oo{i},'AsArray',true);
+    tt(i,:)=t(1,vars);
+end
+disp(tt) % Print list of conditions.
+
+%% PRINT THE RESULTS
+% We skip any condition without all these fields.
+vars={'experiment' 'condition' 'observer' 'trials' 'luminanceAtEye' 'eccentricityXYDeg' 'targetDurationSec' 'targetCyclesPerDeg' 'noiseSD' 'targetHeightDeg' 'targetCyclesPerDeg' 'N' 'contrast'};
+tt=table;
+for oi=1:length(oo)
+    t=struct2table(oo{oi},'AsArray',true);
+    if ~all(ismember({'trials' 'transcript'},t.Properties.VariableNames)) || isempty(t.trials) || t.trials==0
+        % Skip condition without data.
+        continue
+    end
+    if false
+        % Create empty cell for every missing field in the condition.
+        missing=~ismember(vars,t.Properties.VariableNames);
+        missingNames=vars(missing);
+        for i=1:length(missingNames)
+            name=missingNames{i};
+            t.(name)=[];
+        end
+    else
+        % Warn, skip the condition, and report which fields were missing.
+        ok=ismember(vars,t.Properties.VariableNames);
+        if ~all(ok)
+            missing=join(vars(~ok),' ');
+            warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
+            continue
+        end
+    end
+    tt(end+1,:)=t(1,vars);
+end
+disp(tt) % Print the list of conditions, with results.
