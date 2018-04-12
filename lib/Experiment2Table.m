bf01c686b88793dc3e202445facf852acbd17c76
denispelli <denis.pelli@nyu.edu>
2018-04-12 02:21:17 -0400

    pulled code from scripts to make subroutine

---
+++ lib/Experiment2Table.m
@@ -0,0 +1,21 @@
+function tt=Experiment2Table(oo,vars)
+% tt=Experiment2Table(oo,vars);
+% oo is a cell array representing an experiment. Each element represents
+% one block for one condition of the experiment. It's an o struct with many
+% fields, including o.trials.
+% Denis Pelli, April, 2018
+tt=table;
+for oi=1:length(oo)
+    t=struct2table(oo{oi},'AsArray',true);
+    if ~all(ismember({'trials'},t.Properties.VariableNames)) || t.trials==0
+        continue % Skip condition without data.
+    end
+    % Check for and report any missing fields in this condition.
+    ok=ismember(vars,t.Properties.VariableNames);
+    if ~all(ok)
+        missing=join(vars(~ok),' ');
+        warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
+        continue
+    end
+    tt(end+1,:)=t(1,vars);
+end % for oi=1:length(oo)
