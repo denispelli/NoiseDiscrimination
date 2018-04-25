bdc9a227ccd3bf212ccf26411a8900452fee7a19
denispelli <denis.pelli@nyu.edu>
2018-04-25 16:57:14 -0400

    use mincon to force positive E0 and Neq

--- lib/EstimateNeq.m
+++ lib/EstimateNeq.m
@@ -2,7 +2,8 @@ function [E0,Neq]=EstimateNeq(E,N)
 % We estimate the best linear fit of E vs N, taking into account the
 % conservation of sd of log E. The minimization begins with a guess that is
 % a linear regression that doesn't take that into account (i.e. assuming
-% conservation of sd of E, not log E).
+% conservation of sd of E, not log E). We constrain the solution to
+% nonnegative values of E0 and Neq.
 % April, 2018. denis.pelli@nyu.edu
 assert(all(size(N)==size(E)),'E and N must have same size.');
 assert(all(N>=0),'N must be positive.');
@@ -18,22 +19,17 @@ end
 b=[ones(size(N)) N]\E; % Regress.
 E0=b(1); % Guess E0
 Neq=E0/b(2); % Guess Neq
+E0=max(E0,eps); % Impose positivity on the guess, so mincon won't fail.
+Neq=max(Neq,0); % Impose positivity on the guess, so mincon won't fail.
 fun=@(b) Cost(E,N,b(1),b(2));
-b=fminsearch(fun,[E0 Neq]); % Unconstrained fit.
-% b=fmincon(fun,[E0 Neq],[],[],[],[],[0 0],[inf inf]); % Constrain E0 and Neq to not be negative.
+% b=fminsearch(fun,[E0 Neq]); % Unconstrained fit.
+b=fmincon(fun,[E0 Neq],[],[],[],[],[0 0],[inf inf]); % Search constrains E0 and Neq to not be negative.
 E0=b(1);
 Neq=b(2);
-if E0<0 || Neq<0 || ~isfinite(E0) || ~isfinite(Neq)
-    % Given the constrained search above, I don't expect to ever get this
-    % warning.
-    warning('Estimated E0 %.1e or Neq %.1e is not positive and finite.',E0,Neq);
-    E0=max(0,E0);
-    Neq=max(0,Neq);
-end
 end
 
 function cost=Cost(E,N,E0,Neq)
-% Compute RMS error in predicted log E. We use log because the sd of
+% Compute RMS error in predicting log E. We use log because the sd of
 % measurement is approximately conserved in log E, and not in E.
 modelE=(E0/Neq)*(N+Neq);
 cost=sqrt(mean(log10(E./modelE).^2));
