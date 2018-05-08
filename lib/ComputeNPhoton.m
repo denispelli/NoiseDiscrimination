0e58dc62d69c998a6128a552b1dfe6f269d26bc4
denispelli <denis.pelli@nyu.edu>
2018-05-07 22:42:19 -0400

    cope with missing fields

--- lib/ComputeNPhoton.m
+++ lib/ComputeNPhoton.m
@@ -1,5 +1,12 @@
 function oo=ComputeNPhoton(oo)
 o=oo(1);
+if ~isfield(o,'luminanceFactor')
+    [oo.luminanceFactor]=deal(1);
+end
+if ~isfield(o,'filterTransmission')
+    [oo.filterTransmission]=deal(1);
+end
+o=oo(1);
 o.luminanceAtEye=o.luminanceFactor*o.filterTransmission*o.LBackground;
 switch o.eyes
     case {'left' 'right'}
@@ -10,8 +17,25 @@ switch o.eyes
         error('Unknown o.eyes "%s".',o.eyes);
 end
 if isempty(o.pupilDiameterMm)
-    xyDeg=XYDegOfXYPix(o,[0 0])-XYDegOfXYPix(o,o.stimulusRect(3:4));
-    a=abs(xyDeg(1)*xyDeg(2)); % Screen area in deg^2.
+    try
+        % XYDegOfXYPix fails if o.nearPointXYPix is not a field.
+        xyDeg=XYDegOfXYPix(o,[0 0])-XYDegOfXYPix(o,o.stimulusRect(3:4));
+        a=abs(xyDeg(1)*xyDeg(2)); % Screen area in deg^2.
+    catch
+        % This is accurate if the observer is fixating the center of the
+        % display. Even if the observer is looking elsewhere, this is
+        % probably accurate enough for this rough use, especially since we
+        % are giving the area of a luminous field that is assumed to be
+        % centered on fixation. Estimation of pupil diameter without
+        % looking at the pupil is always rough.
+        if ~isfield(o,'screenRect')
+            % Began saving this May 6, 2018.
+            o.screenRect=[0 0 1680 1050]; % MacBook Pro.
+        end
+        x=2*atan2d(0.5*RectWidth(o.screenRect)/o.pixPerCm,o.viewingDistanceCm);
+        y=2*atan2d(0.5*RectHeight(o.screenRect)/o.pixPerCm,o.viewingDistanceCm);
+        a=x*y;
+    end
     if ~isfield(o,'age')
         o.age=20;
         [oo.age]=deal(o.age);
