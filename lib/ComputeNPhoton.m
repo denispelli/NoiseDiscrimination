bc830891aaca9eaaa27026eb6db86a1bda793a32
denispelli <denis.pelli@nyu.edu>
2018-05-08 22:45:16 -0400

    treat each condition separately

--- lib/ComputeNPhoton.m
+++ lib/ComputeNPhoton.m
@@ -1,55 +1,62 @@
 function oo=ComputeNPhoton(oo)
-o=oo(1);
-if ~isfield(o,'filterTransmission')
-    [oo.filterTransmission]=deal(1);
-end
-o=oo(1);
-o.luminanceAtEye=o.filterTransmission*o.LBackground;
-switch o.eyes
-    case {'left' 'right'}
-        e=1;
-    case 'both'
-        e=2;
-    otherwise
-        error('Unknown o.eyes "%s".',o.eyes);
-end
-if isempty(o.pupilDiameterMm)
-    try
-        % XYDegOfXYPix fails if o.nearPointXYPix is not a field.
-        xyDeg=XYDegOfXYPix(o,[0 0])-XYDegOfXYPix(o,o.stimulusRect(3:4));
-        a=abs(xyDeg(1)*xyDeg(2)); % Screen area in deg^2.
-    catch
-        % This is accurate if the observer is fixating the center of the
-        % display. Even if the observer is looking elsewhere, this is
-        % probably accurate enough for this rough use, especially since we
-        % are giving the area of a luminous field that is assumed to be
-        % centered on fixation. Estimation of pupil diameter without
-        % looking at the pupil is always rough.
-        if ~isfield(o,'screenRect')
-            % Began saving this May 6, 2018.
-            o.screenRect=[0 0 1680 1050]; % MacBook Pro.
+for oi=1:length(oo)
+    if ~isfield(oo(oi),'filterTransmission')
+        oo(oi).filterTransmission=1;
+    end
+    oo(oi).luminanceAtEye=oo(oi).filterTransmission*oo(oi).LBackground;
+    switch oo(oi).eyes
+        case {'left' 'right'}
+            e=1;
+        case 'both'
+            e=2;
+        otherwise
+            error('Unknown o.eyes "%s".',oo(oi).eyes);
+    end
+    if isempty(oo(oi).pupilDiameterMm)
+        try
+            % XYDegOfXYPix fails if o.nearPointXYPix is not a field.
+            xyDeg=XYDegOfXYPix(oo(oi),[0 0])-XYDegOfXYPix(oo(oi),oo(oi).stimulusRect(3:4));
+            a=abs(xyDeg(1)*xyDeg(2)); % Screen area in deg^2.
+        catch
+            % This is accurate if the observer is fixating the center of the
+            % display. Even if the observer is looking elsewhere, this is
+            % probably accurate enough for this rough use, especially since we
+            % are giving the area of a luminous field that is assumed to be
+            % centered on fixation. Estimation of pupil diameter without
+            % looking at the pupil is always rough.
+            if ~isfield(oo(oi),'screenRect') || isempty(oo(oi).screenRect)
+                % Began saving this May 6, 2018.
+                oo(oi).screenRect=[0 0 1680 1050]; % MacBook Pro.
+            end
+            x=2*atan2d(0.5*RectWidth(oo(oi).screenRect)/oo(oi).pixPerCm,oo(oi).viewingDistanceCm);
+            y=2*atan2d(0.5*RectHeight(oo(oi).screenRect)/oo(oi).pixPerCm,oo(oi).viewingDistanceCm);
+            a=x*y;
         end
-        x=2*atan2d(0.5*RectWidth(o.screenRect)/o.pixPerCm,o.viewingDistanceCm);
-        y=2*atan2d(0.5*RectHeight(o.screenRect)/o.pixPerCm,o.viewingDistanceCm);
-        a=x*y;
+        if ~isfield(oo(oi),'age')
+            oo(oi).age=20;
+        end
+        mm=PupilDiameter(oo(oi).LBackground,a,oo(oi).age,e);
+        oo(oi).pupilDiameterMm=mm;
+        oo(oi).pupilKnown=false;
     end
-    if ~isfield(o,'age')
-        o.age=20;
-        [oo.age]=deal(o.age);
+    oo(oi).retinalIlluminanceTd=oo(oi).luminanceAtEye*pi*oo(oi).pupilDiameterMm^2/4;
+    if ~isfield(oo(oi),'A') || isempty(oo(oi).A)
+        switch oo(oi).targetKind
+            case {'letter' 'image'}
+                oo(oi).A=oo(oi).targetHeightDeg^2;
+            case {'gabor'}
+                oo(oi).targetCyclesPerDeg=oo(oi).targetGaborCycles/oo(oi).targetHeightDeg;
+                oo(oi).A=oo(oi).targetCyclesPerDeg^-2;
+        end
     end
-    mm=PupilDiameter(o.LBackground,a,o.age,e);
-    o.pupilDiameterMm=mm;
-    o.pupilKnown=false;
-end
-o.retinalIlluminanceTd=o.luminanceAtEye*pi*o.pupilDiameterMm^2/4;
-% Compute equivalent input noise for 100% transduction efficiency.
-q=1.26e6;
-o.NPhoton=1/(q*o.retinalIlluminanceTd*e);
-[oo.luminanceAtEye]=deal(o.luminanceAtEye);
-[oo.retinalIlluminanceTd]=deal(o.retinalIlluminanceTd);
-[oo.pupilDiameterMm]=deal(o.pupilDiameterMm);
-[oo.NPhoton]=deal(o.NPhoton);
-end
+    if ~isfield(oo(oi),'LAT') || isempty(oo(oi).LAT)
+        oo(oi).LAT=oo(oi).retinalIlluminanceTd*oo(oi).A*oo(oi).targetDurationSecs;
+    end
+    % Compute equivalent input noise for 100% transduction efficiency.
+    q=1.26e6;
+    oo(oi).NPhoton=1/(q*oo(oi).retinalIlluminanceTd*e);
+end % for oi=1:length(oo)
+end % function
 
 function xyDeg=XYDegOfXYPix(o,xyPix)
 % Convert position from (x,y) coordinate in o.stimulusRect to deg (relative
