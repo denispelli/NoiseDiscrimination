7774f0113f29c68cabf6fc5857807c7384c6d51f
denispelli <denis.pelli@nyu.edu>
2018-05-11 01:04:04 -0400

    simplified the code. might be faster too.

--- lib/PsychRandSample.m
+++ lib/PsychRandSample.m
@@ -8,9 +8,10 @@ function x=PsychRandSample(list,dims)
 % from a bounded Gaussian distribution. Also see RAND, RANDN, Randi,
 % Sample, and Shuffle.
 % 
-% "list" can be a double, char, cell, or struct array, but it must be a 
-% vector (1xn or nx1). In the future, we may accept matrices (mxn) and treat
-% columns separately, as other Matlab functions do.
+% "list" can be a double, char, cell, or struct array (e.g. of strings),
+% but it must be a vector (1xn or nx1). In the future, we may accept
+% matrices (mxn) and treat columns separately, as other Matlab functions
+% do.
 
 % Denis Pelli 7/22/97 3/24/98
 % 8/14/99 dgp Renamed from "Rands" (which conflicts with Neural Net Toolbox) to
@@ -20,6 +21,9 @@ function x=PsychRandSample(list,dims)
 % 7/24/04 awi Cosmetic.
 % 12/13/04 dgp & kat Enhanced to support cell arrays.
 % 1/22/2012 dgp renamed from RandSample to PsychRandSample to avoid conflict with new MATLAB function.
+% 5/10/18 dgp Simplified the code. It might be faster too. The runtime of
+% the two active lines are similar. I discovered that the last line works
+% for cell lists as well as number lists.
 
 if nargin<1 || nargin>2 || min(size(list))~=1
 	error('Usage: x=PsychRandSample(list,[dims])')
@@ -27,9 +31,7 @@ end
 if nargin==1
 	dims=[1 1];
 end
-i=ceil(length(list)*rand(dims)); % equivalent to calling Randi. % TAKES 1.5 ms.
-if iscell(list)
-	x=reshape({list{i(:)}},dims); % ?
-else
-	x=reshape(list(i(:)),dims); % TAKES 1 ms.
-end
+% Generate random subscripts.
+i=randi(length(list),dims); 
+% Use them.
+x=list(i);
