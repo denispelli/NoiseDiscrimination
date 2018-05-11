2ab9c37acc9f8d91747601d8dca5cdbea121bd36
denispelli <denis.pelli@nyu.edu>
2018-05-11 12:54:47 -0400

    reveals how MATLAB formats in Command Window

---
+++ debug/matlab formatting.m
@@ -0,0 +1,11 @@
+<strong>, Type Y for yes use it. Hit RETURN to ignore it, or DELETE to delete it:
+Scalar structure required for this assignment.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('OfferToResumeExperiment2', '/Users/denispelli/Dropbox/NoiseDiscrimination/lib/OfferToResumeExperiment2.m', 56)" style="font-weight:bold">OfferToResumeExperiment2</a> (<a href="matlab: opentoline('/Users/denispelli/Dropbox/NoiseDiscrimination/lib/OfferToResumeExperiment2.m',56,0)">line 56</a>)
+                        ooo{j}.quitRun=false;
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('RunExperiment2', '/Users/denispelli/Dropbox/NoiseDiscrimination/lib/RunExperiment2.m', 59)" style="font-weight:bold">RunExperiment2</a> (<a href="matlab: opentoline('/Users/denispelli/Dropbox/NoiseDiscrimination/lib/RunExperiment2.m',59,0)">line 59</a>)
+ooo=OfferToResumeExperiment2(ooo);
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('csfRun', '/Users/denispelli/Dropbox/NoiseDiscrimination/csfRun.m', 150)" style="font-weight:bold">csfRun</a> (<a href="matlab: opentoline('/Users/denispelli/Dropbox/NoiseDiscrimination/csfRun.m',150,0)">line 150</a>)
+ooo=RunExperiment2(ooo);
