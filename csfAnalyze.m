385939e7d45853440ec039f2fe3724fd381e0b97
denispelli <denis.pelli@nyu.edu>
2018-05-13 13:18:39 -0400

    analyze csf results

---
+++ csfAnalyze.m
@@ -0,0 +1,288 @@
+%% Analyze the data collected by csfRun.
+global printConditions
+readExperiment=true;
+printConditions=false;
+printFilenames=true;
+plotGraphs=true;
+myPath=fileparts(mfilename('fullpath')); % Takes 0.1 s.
+addpath(fullfile(myPath,'lib')); % Folder in same directory as this M file.
+experiment='csf';
+dataFolder=fullfile(fileparts(mfilename('fullpath')),'data');
+cd(dataFolder);
+if readExperiment
+    % Each file contains an experiment. One cell per block. One struct
+    % array per block, with an element per condition and its threshold.
+    matFiles=dir(fullfile(dataFolder,'EvsN-*.mat'));
+else
+    % Each file contains one condition and its threshold.
+    matFiles=dir(fullfile(dataFolder,[experiment '*-NoiseDiscrimination*.mat']));
+end
+close all
+% We will ignore the blocking and read every condition into one huge struct
+% array oo, with one element per condition and its threshold.
+oo=[];
+clear Plot % To clear the persistent variables in the subroutine below.
+oi=0;
+vars={'functionNames' 'conditionName' 'experiment' 'dataFilename' ...
+   'experimenter' 'observer' 'trials' ...
+   'targetKind' 'targetGaborPhaseDeg' 'targetGaborCycles' 'targetCyclesPerDeg'...
+   'targetHeightDeg' 'targetDurationSecs' 'targetDurationSecsMean' 'targetDurationSecsSD'...
+   'targetCheckDeg' 'fullResolutionTarget' ...
+   'noiseType' 'noiseSD'  'noiseCheckDeg' ...
+   'eccentricityXYDeg' 'viewingDistanceCm' 'eyes' ...
+   'contrast' 'E' 'N' 'LBackground' 'luminanceAtEye' 'luminanceFactor'...
+   'filterTransmission' 'useFilter' 'retinalIlluminanceTd' 'pupilDiameterMm'...
+   'pixPerCm' 'nearPointXYPix' 'trialsPerBlock'};
+for iFile=1:length(matFiles) % One file per iteration.
+   % Extract the desired fields into "oo", one cell-row per threshold.
+   d=load(matFiles(iFile).name);
+   if readExperiment
+       if contains(matFiles(iFile).name,'NoiseDiscrimination') || ~isfield(d,'ooo')
+           continue
+       end
+       if ~contains(d.ooo{1}(1).functionNames,experiment)
+           fprintf('Skipping function name: %s\n',d.ooo{1}(1).functionNames);
+           continue
+       end
+       ooo=d.ooo;
+   else
+      if ~isfield(d,'o') || d.o.trials<10
+         continue
+      end
+      ooo={d.o};
+   end
+   for block=1:length(ooo)
+      ooIn=ooo{block};
+      for oiIn=1:length(ooIn)
+         oi=oi+1;
+         for field=vars
+            if isfield(ooIn(oiIn),field{1})
+               newField=field{1};
+               oo(oi).(newField)=ooIn(oiIn).(field{1});
+            else
+               if block==1
+                  warning OFF BACKTRACE
+                  warning('Missing o field: %s\n',field{:});
+               end
+            end
+         end
+      end
+   end
+end
+if isempty(oo)
+   fprintf('Couldn''t find any data.\n');
+   return
+end
+%% DISCARD THRESHOLDS WITH INSUFFICIENT TRIALS
+s=sprintf('condition(trials):');
+for oi=length(oo):-1:1
+    if isempty(oo(oi).trials) || oo(oi).trials<oo(oi).trialsPerBlock
+        s=[s sprintf(' %d(%d),',oi,oo(oi).trials)];
+        oo(oi)=[];
+    end
+end
+if sum([oo.trials]<oo(1).trialsPerBlock)>0
+    warning('Discarding %d threshold(s) with fewer than %d trials: %s',sum([oo.trials]<oo(1).trialsPerBlock),oo(oi).trialsPerBlock,s);
+end
+
+%% COMPUTE DERIVED QUANTITIES
+oo=ComputeNPhoton(oo);
+for conditionName=unique({oo.conditionName})
+    ii=ismember({oo.conditionName},conditionName);
+    E=[oo(ii).E];
+    N=[oo(ii).N];
+    [E0,Neq]=EstimateNeq(E,N);
+    [oo(ii).E0]=deal(E0);
+    [oo(ii).Neq]=deal(Neq);
+end
+
+% PRINT
+t=struct2table(oo);
+if printFilenames
+    t(:,{'dataFilename','experiment','conditionName','observer','targetCyclesPerDeg','eccentricityXYDeg','contrast','noiseSD' 'Neq' 'E0'})
+end
+return
+
+if plotGraphs
+    fprintf('Plotting %d thresholds.\n',length(oo));
+    for observer=unique({oo.observer})
+        isObserver=ismember({oo.observer},observer);
+        which=isObserver;
+        if sum(which)>0
+            fprintf('%s-%s-%s: %d thresholds.\n',observer{1},conditionName{1},noiseType{1},sum(which));
+            subplots=[1 length(unique({oo.conditionName}))];
+            [~,subplotIndex]=ismember(conditionName,unique({oo.conditionName}));
+            Plot(oo(which),subplots,subplotIndex);
+        end
+    end
+end
+return
+
+function Plot(oo,subplots,subplotIndex)
+global printConditions
+persistent previousObserver figureHandle overPlots figureTitle axisHandle
+if isempty(oo)
+    return
+end
+fontSize=12*0.6;
+if isempty(get(groot,'CurrentFigure')) || ~streq(oo(1).observer,previousObserver)
+    previousObserver=oo(1).observer;
+    rect=Screen('Rect',0);
+    figureTitle=[oo(1).experiment '-' oo(1).observer];
+    figureHandle=figure('Name',figureTitle,'NumberTitle','off','pos',[10 10 900 300]);
+    orient 'landscape'; % For printing.
+    overPlots=zeros(1,subplots(1)*subplots(2));
+    axisHandle=zeros(1,subplots(1)*subplots(2));
+    an=annotation('textbox',[0 0.9 1 .1],...
+        'String',figureTitle,...
+        'LineStyle','none','FontSize',fontSize*2,...
+        'HorizontalAlignment','center','VerticalAlignment','top');
+else
+    figure(figureHandle);
+end
+if axisHandle(subplotIndex)==0
+    % subplot(m,n,p) makes it easy to show several related graphs in one
+    % figure window. Calling subplot(m,n,p) for an existing axis object
+    % seems to erase it. So, when we first select that figure panel, we
+    % save a handle to it, which we later reuse to select the panel by
+    % calling subplot(handle), without calling subplot(m,n,p) again.
+    axisHandle(subplotIndex)=subplot(subplots(1),subplots(2),subplotIndex);
+else
+    hold(axisHandle(subplotIndex),'on');
+    subplot(axisHandle(subplotIndex));
+end
+
+% Sort by eccentricity.
+[~,ii]=sort(oo.eccentricityXYDeg(1));
+oo=oo(ii);
+% Sort by sf.
+[~,ii]=sort(oo.targetCyclesPerDeg);
+oo=oo(ii);
+
+
+%% Create CSV file
+vars={'experiment' 'conditionName' ...
+    'experimenter' 'observer' 'trials' 'contrast' 'luminanceAtEye' 'E' 'N' ...
+    'targetKind' 'targetCyclesPerDeg'  'targetHeightDeg'  'targetDurationSecs' ...
+    'noiseType' 'noiseSD'  'noiseCheckDeg' ...
+    'eccentricityXYDeg' 'viewingDistanceCm' 'eyes' ...
+    'LBackground'  'dataFilename'};
+t=struct2table(oo);
+spreadsheet=fullfile(fileparts(mfilename('fullpath')),'data',[oo(1).experiment '.csv']);
+% writetable(t,spreadsheet);
+if printConditions
+    disp(t(:,vars));
+end
+fprintf('All selected fields have been saved in spreadsheet: /data/%s.csv\n',oo(1).experiment);
+
+return
+
+%% Plot
+if Neq>=min(N) && Neq<2*max(N)
+    % Trust reasonable Neq. 
+    NLow=Neq/100;
+    NHigh=max([N Neq*10]);
+else
+    % Igore crazy Neq. 
+    NLow=min(N(N>0)); % Smallest nonzero noise.
+    NHigh=max(N);
+end
+overPlots(subplotIndex)=overPlots(subplotIndex)+1;
+switch overPlots(subplotIndex)
+    case 1
+        style1='xk';
+        style2='-k';
+        hold off;
+    case 2
+        style1='ok';
+        style2='--k';
+        hold on;
+    case 3
+        style1='+k';
+        style2=':k';
+        hold on;
+    otherwise
+        style1='^k';
+        style2='-.k';
+        hold on;
+end
+legendText=sprintf('%s %s',oo(1).conditionName,oo(1).noiseType);
+loglog(max(N,NLow),E,style1,'DisplayName',legendText);
+hold on;
+ax=gca;
+NLine=logspace(log10(NLow),log10(NHigh));
+ELine=(NLine+Neq)*E0/Neq;
+loglog(NLine,ELine,style2,'DisplayName','Linear fit');
+set(gca,'FontSize',fontSize);
+title(oo(1).conditionName);
+xlabel('\it N \rm (s deg^2)','Interpreter','tex')
+ylabel('\it E \rm (s deg^2)','Interpreter','tex');
+lgd=legend('show');
+lgd.Location='northwest';
+lgd.FontSize=fontSize;
+legend('boxoff');
+oo=ComputeNPhoton(oo);
+caption={};
+caption{1}=sprintf('experimenter %s, observer %s, eyes %s', ...
+    oo(1).experimenter,oo(1).observer,oo(1).eyes);
+caption{2}=sprintf('noiseSD<=%.2f, noiseCheckDeg %.3f, noiseType %s', ...
+    max([oo.noiseSD]),oo(1).noiseCheckDeg,oo(1).noiseType);
+caption{3}=sprintf('%.1f cd/m^2, %.0f td, LAT %.2f, log NPhoton %.1f', ...
+    oo(1).luminanceAtEye,oo(1).retinalIlluminanceTd,oo(1).LAT,log10(oo(1).NPhoton));
+caption{4}=sprintf('ecc. [%.0f %.0f] deg, %.1f s, %s %.1f c/deg, log Neq %.2f',...
+    oo(1).eccentricityXYDeg,oo(1).targetDurationSecs,oo(1).targetKind,oo(1).targetCyclesPerDeg,log10(oo(1).Neq));
+text(0.02,.02,caption,'Units','normalized','FontSize',fontSize,'VerticalAlignment','bottom');
+% Set lower Y limit to E0/40. This leaves room for the "caption" text at
+% bottom of graph. If necessary, expand Y range to 3 log units.
+logUnits=3;
+ax=gca;
+yLimits=ax.YLim;
+yLimits(1)=E0/40;
+r=diff(log10(yLimits)); % Number of log units
+if logUnits>r
+   yLimits(2)=yLimits(2)*10^(logUnits-r);
+end
+ax.YLim=yLimits;
+
+ax=gca;
+if ax.XLim(1)<=0
+    warning('Lower X limit too low!! Setting it to NLow.')
+    xLim=ax.XLim
+    ax.XLim=[NLow xLim(2)];
+    oo(1).observer
+    oo(1).experiment
+    oo(1).conditionName
+    oo(1).noiseType
+    NLow
+    min(NLine)
+    E0
+    Neq
+end
+
+% Scale log unit to be 1.5 cm, vertically and horizontally.
+ax=gca;
+u=ax.Units;
+ax.Units='centimeters';
+drawnow; % Needed for valid Position reading.
+pos=ax.Position;
+ax.Position=[pos(1:2) 1.5*diff(log10(ax.XLim)) 1.5*diff(log10(ax.YLim))];
+ax.Units=u;
+
+% Add second x-axis for noise contrast noiseSD.
+% ax1=gca;
+% ax2=axes('Position',ax1.Position,...
+%     'XAxisLocation','top',...
+%     'YAxisLocation','right',...
+%     'Color','none',...
+%     'XColor','k','YColor','k');
+% ax2.XScale=ax1.XScale;
+% ax2.YScale=ax1.YScale;
+% ax2.XLim=sqrt(ax1.XLim)*oo(end).noiseSD/sqrt(oo(end).N);
+% ax2.YLim=ax1.YLim;
+% ax2.FontSize=ax1.FontSize;
+
+% Save plot to disk
+graphFile=fullfile(fileparts(mfilename('fullpath')),'data',[figureTitle '.eps']);
+saveas(gcf,graphFile,'epsc')
+% print(gcf,graphFile,'-depsc'); % equivalent to saveas above
+end
