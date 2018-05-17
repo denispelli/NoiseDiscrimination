a12bc87834e773fa6743e710200aa5d11209675b
denispelli <denis.pelli@nyu.edu>
2018-05-17 11:05:14 -0400

    remove code now in MaxNoiseSD

--- EvsNRun.m
+++ EvsNRun.m
@@ -10,18 +10,6 @@
 % P=0.75, assuming 4 alternatives
 % monocular, temporal field, right eye
 
-%% MAX SD OF EACH NOISE TYPE
-% With the same bound on range, we can reach 3.3 times higher noiseSd using
-% binary instead of gaussian noise. In the code below, we use steps of
-% 2^0.5=1.4, so i increase noiseSd by a factor of 2^1.5=2.8 when using
-% binary noise.
-sdOverBound.gaussian=0.43;
-sdOverBound.uniform=0.58;
-sdOverBound.binary=1.41;
-maxBound=0.37; % Rule of thumb based on experience with gaussian.
-maxSd=struct('gaussian',maxBound*sdOverBound.gaussian,'uniform',maxBound*sdOverBound.uniform,'binary',maxBound*sdOverBound.binary);
-% maxSd
-
 %% GET READY
 clear o oo
 o.questPlusEnable=false;
