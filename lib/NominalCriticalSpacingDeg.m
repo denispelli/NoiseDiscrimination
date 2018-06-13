a26021eb55b942f7b8c829347bbdcbb7f2b5408c
denispelli <denis.pelli@nyu.edu>
2018-06-13 16:01:41 -0400

    polish comments, error checking, add new name

--- lib/NominalCriticalSpacingDeg.m
+++ lib/NominalCriticalSpacingDeg.m
@@ -1,6 +1,11 @@
-function nominalCriticalSpacingDeg=NominalCriticalSpacingDeg(eccentricityXYDeg)
+function criticalSpacingDeg=NominalCriticalSpacingDeg(eccentricityXYDeg)
 % Eq. 14 from Song, Levi, and Pelli (2014).
-% Modified x intercept by Pelli et all. 2017.
+% The argument can be a scalar radial eccentricity in deg, or a vector of x
+% and y eccentricity in deg.
+% Modified x intercept by Pelli et al. 2017.
 % See also: NominalAcuityDeg
-radialEccDeg=sqrt(sum(eccentricityXYDeg.^2))
-nominalCriticalSpacingDeg=0.3*(radialEccDeg+0.15);
+if length(eccentricityXYDeg)<1 || length(eccentricityXYDeg)>2
+    error('The eccentricityXYDeg argument must be a scalar (radial) or an x y vector (cartesian).');
+end
+radialEccDeg=sqrt(sum(eccentricityXYDeg.^2));
+criticalSpacingDeg=0.3*(radialEccDeg+0.15);
--- lib/NominalCrowdingDistanceDeg.m
+++ lib/NominalCrowdingDistanceDeg.m
@@ -1,11 +1,11 @@
-function acuityDeg=NominalAcuityDeg(eccentricityXYDeg)
-% Eq. 13 from Song, Levi, and Pelli (2014).
+function crowdingDistanceDeg=NominalCrowdingDistanceDeg(eccentricityXYDeg)
+% Eq. 14 from Song, Levi, and Pelli (2014).
 % The argument can be a scalar radial eccentricity in deg, or a vector of x
 % and y eccentricity in deg.
-% See also: NominalCriticalSpacingDeg
+% Modified x intercept by Pelli et al. 2017.
+% See also: NominalAcuityDeg
 if length(eccentricityXYDeg)<1 || length(eccentricityXYDeg)>2
     error('The eccentricityXYDeg argument must be a scalar (radial) or an x y vector (cartesian).');
 end
 radialEccDeg=sqrt(sum(eccentricityXYDeg.^2));
-acuityDeg=0.029*(radialEccDeg+2.72);
-
+crowdingDistanceDeg=0.3*(radialEccDeg+0.15);
