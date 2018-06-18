b05f16f25a0f97af1c84935f372a4d0851419939
denispelli <denis.pelli@nyu.edu>
2018-06-18 04:32:53 -0400

    o=oo{oi} may now be an array.

--- lib/Experiment2Table.m
+++ lib/Experiment2Table.m
@@ -2,12 +2,12 @@ function tt=Experiment2Table(oo,vars)
 % tt=Experiment2Table(oo,vars);
 % oo is a cell array representing an experiment. Each element represents
 % one block for one condition of the experiment. It's an o struct with many
-% fields, including o.trials.
+% fields, including o.trials. Each o may be an array.
 % Denis Pelli, April, 2018
 tt=table;
 for oi=1:length(oo)
     t=struct2table(oo{oi},'AsArray',true);
-    if ~all(ismember({'trials'},t.Properties.VariableNames)) || t.trials==0
+    if ~all(ismember({'trials'},t.Properties.VariableNames)) || all(t.trials==0)
         continue % Skip condition without data.
     end
     % Check for and report any missing fields in this condition.
@@ -17,5 +17,5 @@ for oi=1:length(oo)
         warning('Skipping incomplete condition %d, because it lacks: %s',i,missing{1});
         continue
     end
-    tt(end+1,:)=t(1,vars);
+    tt=vertcat(tt,t(:,vars));
 end % for oi=1:length(oo)
