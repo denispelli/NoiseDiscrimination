6e29a62ba3c494c7f26612fcece9bd7a9a12f987
denispelli <denis.pelli@nyu.edu>
2019-03-19 00:34:17 -0400

    copied from CriticalSpacing project

---
+++ lib/IsXYInRect.m
@@ -0,0 +1,10 @@
+function isTrue=IsXYInRect(xy,rect)
+% isTrue=IsXYInRect(xy,rect)
+if nargin~=2
+    error('Need two args for function isTrue=IsXYInRect(xy,rect)');
+end
+if size(xy)~=[1 2]
+    error('First arg to IsXYInRect(xy,rect) must be [x y] pair.');
+end
+isTrue=IsInRect(xy(1),xy(2),rect);
+end
