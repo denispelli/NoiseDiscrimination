35ad9f2d30f541b7720e599b445a200d5ac899f6
denispelli <denis.pelli@nyu.edu>
2019-03-20 07:08:30 -0400

    cosmetic

--- lib/MFileLineNr.m
+++ lib/MFileLineNr.m
@@ -1,7 +1,8 @@
-function LineNr = MFileLineNr()
+function line = MFileLineNr()
+% line=MFileLineNr;
 % MFILELINENR returns the current linenumber
     Stack  = dbstack;
-    LineNr = Stack(2).line;   % the line number of the calling function
+    line = Stack(2).line;   % the line number of the calling function
 end
 
 % Downloaded from MATLAB Central in November 2015
