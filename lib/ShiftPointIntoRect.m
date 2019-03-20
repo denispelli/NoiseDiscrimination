c49cca858420cbc2595caa9de300e0a553f5525b
denispelli <denis.pelli@nyu.edu>
2019-03-20 07:10:52 -0400

    cosmetic

--- lib/ShiftPointIntoRect.m
+++ lib/ShiftPointIntoRect.m
@@ -1,22 +1,25 @@
 function o=ShiftPointIntoRect(o,ff,name,xy,radiusDeg,r)
 % o=ShiftPointIntoRect(o,ff,name,xy,radiusDeg,r)
+% If necessary and o.okToShiftCoordinates then adjust the near-point
+% eccentricity o.nearPointXYDeg to fit an object (e.g. the target) with
+% specified eccentricity (corresponding to xy) fits on the screen.
 radius=o.pixPerDeg*radiusDeg;
 r=InsetRect(r,radius,radius);
 if ~IsXYInRect(xy,r) % Is fixation off screen?
     if o.okToShiftCoordinates
-        % Adjust position of near point visual coordinate so "name" fits on
+        % Adjust near-point eccentricity o.nearPointXYDeg so "name" fits on
         % screen. Place it at nearest visible point of screen.
         newXY=LimitXYToRect(xy,r);
         % Update o.nearPointXYDeg.
         oldNearPointXYDeg=o.nearPointXYDeg;
         o.nearPointXYDeg=o.nearPointXYDeg+XYDegOfXYPix(o,newXY)-XYDegOfXYPix(o,xy);
-        ffprintf(ff,['NOTE: Adjusting o.nearPointXYDeg from ' ...
+        ffprintf(ff,['NOTE: Adjusting near-point eccentricity o.nearPointXYDeg from ' ...
             '[%.1f %.1f] deg to [%.1f %.1f] deg to fit %s ' ...
             '(with %.1f deg radius) on screen.'],...
             oldNearPointXYDeg,o.nearPointXYDeg,name,radiusDeg);
     else
-        error(['Sorry, despite your request, %s doesn''t fit on screen ' ...
+        error(['Sorry, %s doesn''t fit on screen ' ...
             'and it''s not o.okToShiftCoordinates. You might try ' ...
-            'adjusting o.nearPointXYDeg.'],name);
+            'adjusting the near-point eccentricity o.nearPointXYDeg yourself.'],name);
     end
 end
