0677d4fa6b71cacfe54078eab323f25a6f4ac73c
denispelli <denis.pelli@nyu.edu>
2019-04-24 01:06:14 -0400

    allow vector argument

--- lib/NominalCriticalSpacingDeg.m
+++ lib/NominalCriticalSpacingDeg.m
@@ -1,11 +1,10 @@
-function criticalSpacingDeg=NominalCriticalSpacingDeg(eccentricityXYDeg)
+function nominalCriticalSpacingDeg=NominalCriticalSpacingDeg(eccentricityXYDeg)
 % Eq. 14 from Song, Levi, and Pelli (2014).
-% The argument can be a scalar radial eccentricity in deg, or a vector of x
-% and y eccentricity in deg.
-% Modified x intercept by Pelli et al. 2017.
+% Revised to match foveal measurement of Pelli et al. (2016).
+% You can pass one or more eccentricities. Each row is one eccentricityy.
+% The result is a column with one value per row.
+% This is identical to NominalCrowdingDistanceDeg.
 % See also: NominalAcuityDeg
-if length(eccentricityXYDeg)<1 || length(eccentricityXYDeg)>2
-    error('The eccentricityXYDeg argument must be a scalar (radial) or an x y vector (cartesian).');
-end
-radialEccDeg=sqrt(sum(eccentricityXYDeg.^2));
-criticalSpacingDeg=0.3*(radialEccDeg+0.15);
+assert(size(eccentricityXYDeg,2)==2)
+ecc=sqrt(sum(eccentricityXYDeg.^2,2));
+nominalCriticalSpacingDeg=0.3*(ecc+0.15);
--- lib/NominalCrowdingDistanceDeg.m
+++ lib/NominalCrowdingDistanceDeg.m
@@ -1,11 +1,16 @@
-function crowdingDistanceDeg=NominalCrowdingDistanceDeg(eccentricityXYDeg)
+function nominalCrowdingDistanceDeg=NominalCrowdingDistanceDeg(eccentricityXYDeg)
 % Eq. 14 from Song, Levi, and Pelli (2014).
-% The argument can be a scalar radial eccentricity in deg, or a vector of x
-% and y eccentricity in deg.
-% Modified x intercept by Pelli et al. 2017.
+% Revised to match foveal measurement of Pelli et al. (2016).
 % See also: NominalAcuityDeg
-if length(eccentricityXYDeg)<1 || length(eccentricityXYDeg)>2
-    error('The eccentricityXYDeg argument must be a scalar (radial) or an x y vector (cartesian).');
-end
-radialEccDeg=sqrt(sum(eccentricityXYDeg.^2));
-crowdingDistanceDeg=0.3*(radialEccDeg+0.15);
+% You can pass one or more eccentricities. Each row is one eccentricity.
+% The result is a column with one value per row.
+assert(size(eccentricityXYDeg,2)==2)
+ecc=sqrt(sum(eccentricityXYDeg.^2,2));
+nominalCrowdingDistanceDeg=0.3*(ecc+0.15);
+% ecc= (nominal/0.3)-0.15;
+% If nominal is 0.2 or 1 deg, then critical ecc. is 0.5 or 3 deg.
+% Thus 0.2 deg monospaced tesxt will be read within ecc <0.5 deg.
+% 1 deg text will have a critical ecc. of 3 deg.
+% 2 deg text will have a critical ecc. of 7 deg.
+% Thus most of the visual span will be more than 1 deg of fixation.
+% And the visual span will depend on crowding distance at 7 deg.
