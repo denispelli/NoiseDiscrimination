564907a0afbb74931e7fb7c4f8ef79c366583d11
denispelli <denis.pelli@nyu.edu>
2019-05-06 02:01:55 -0400

    check arguments

--- lib/XYDegOfXYPix.m
+++ lib/XYDegOfXYPix.m
@@ -1,10 +1,15 @@
 function xyDeg=XYDegOfXYPix(o,xyPix)
-% Convert position from (x,y) coordinate in o.stimulusRect to deg (relative
-% to fixation). Deg increase right and up. Pix are in Apple screen
-% coordinates which increase down and right. The perspective transformation
-% is relative to location of near point, which is orthogonal to line of
-% sight. We typically put the target at the near point, but that is not
-% assumed in this routine.
+% Convert position from (x,y) screen coordinate in o.stimulusRect to deg
+% (relative to fixation). Deg increase right and up. Pix are in Apple
+% screen coordinates which increase down and right. The perspective
+% transformation is relative to location of near point, which is orthogonal
+% to line of sight. We typically put the target at the near point, but that
+% is not assumed in this routine.
+assert(isfield(o,'nearPointXYDeg'));
+assert(length(o.nearPointXYDeg)==2);
+assert(isfield(o,'nearPointXYPix'));
+assert(length(o.nearPointXYPix)==2);
+assert(length(xyPix)==2);
 if isempty(o.nearPointXYPix)
     error('You must set o.nearPointXYPix before calling XYDegOfXYPix.');
 end
