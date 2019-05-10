e8062893770de834d1453f40cae5aaad8943217c
denispelli <denis.pelli@nyu.edu>
2019-05-10 09:42:46 -0400

    update to modern dot notation

--- lib/DecadesEqual.m
+++ lib/DecadesEqual.m
@@ -5,23 +5,21 @@ function DecadesEqual(hAxes,xLimits,yLimits)
 % Posted on Stackoverflow by Ken Eaton. Nov 9, 2010.
 % http://stackoverflow.com/questions/4133510/axis-equal-in-a-matlab-loglog-plot
 % https://www.linkedin.com/in/kenneth-eaton-4a9704126/
+% May 2019, Replaced set and get calls by the newer dot notation for XLim
+% and YLim.
 
 if (nargin < 2) || isempty(xLimits)
-   xLimits = get(hAxes,'XLim');
+   xLimits = hAxes.XLim;
 end
 if (nargin < 3) || isempty(yLimits)
-   yLimits = get(hAxes,'YLim');
+   yLimits = hAxes.YLim;
 end
-
-%   logScale = diff(yLimits)/diff(xLimits);
-%   powerScale = diff(log10(yLimits))/diff(log10(xLimits));
-%   set(hAxes,'Xlim',xLimits,...
-%             'YLim',yLimits,...
-%             'DataAspectRatio',[1 logScale/powerScale 1]);
-
-yDecade = diff(yLimits)/diff(log10(yLimits));  %# Average y decade size
-xDecade = diff(xLimits)/diff(log10(xLimits));  %# Average x decade size
-set(hAxes,'XLim',xLimits,'YLim',yLimits,...
-   'DataAspectRatio',[1 yDecade/xDecade 1]);
+yDecade = diff(yLimits)/diff(log10(yLimits));  % Average y decade size
+xDecade = diff(xLimits)/diff(log10(xLimits));  % Average x decade size
+% set(hAxes,'XLim',xLimits,'YLim',yLimits,...
+%    'DataAspectRatio',[1 yDecade/xDecade 1]);
+hAxes.XLim=xLimits;
+hAxes.YLim=yLimits;
+hAxes.DataAspectRatio=[1 yDecade/xDecade 1];
 end
 
