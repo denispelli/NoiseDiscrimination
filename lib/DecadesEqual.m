2bbe25ddfb0cb8a4ad106c9e5eba8475a213d5e4
denispelli <denis.pelli@nyu.edu>
2019-05-10 23:03:42 -0400

    cosmetic

--- lib/DecadesEqual.m
+++ lib/DecadesEqual.m
@@ -1,7 +1,7 @@
 
-function DecadesEqual(hAxes,xLimits,yLimits)
+function DecadesEqual(ax,xLimits,yLimits)
 %DECADESEQUAL Equate x-y log unit length in MATLAB plot.
-% DecadesEqual(hAxes,xLimits,yLimits);
+% DecadesEqual(ax,xLimits,yLimits);
 % Posted on Stackoverflow by Ken Eaton. Nov 9, 2010.
 % http://stackoverflow.com/questions/4133510/axis-equal-in-a-matlab-loglog-plot
 % https://www.linkedin.com/in/kenneth-eaton-4a9704126/
@@ -9,17 +9,17 @@ function DecadesEqual(hAxes,xLimits,yLimits)
 % and YLim.
 
 if (nargin < 2) || isempty(xLimits)
-   xLimits = hAxes.XLim;
+   xLimits = ax.XLim;
 end
 if (nargin < 3) || isempty(yLimits)
-   yLimits = hAxes.YLim;
+   yLimits = ax.YLim;
 end
 yDecade = diff(yLimits)/diff(log10(yLimits));  % Average y decade size
 xDecade = diff(xLimits)/diff(log10(xLimits));  % Average x decade size
-% set(hAxes,'XLim',xLimits,'YLim',yLimits,...
+% set(ax,'XLim',xLimits,'YLim',yLimits,...
 %    'DataAspectRatio',[1 yDecade/xDecade 1]);
-hAxes.XLim=xLimits;
-hAxes.YLim=yLimits;
-hAxes.DataAspectRatio=[1 yDecade/xDecade 1];
+ax.XLim=xLimits;
+ax.YLim=yLimits;
+ax.DataAspectRatio=[1 yDecade/xDecade 1];
 end
 
