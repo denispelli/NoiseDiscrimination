2b2fde1f5337d106df1a6f3f6653935e59e44d08
denispelli <denis.pelli@nyu.edu>
2019-05-13 08:01:00 -0400

    cosemtic

--- lib/IndexOfLuminance.m
+++ lib/IndexOfLuminance.m
@@ -3,13 +3,16 @@ function image = IndexOfLuminance(cal,imageLuminance)
 % IndexOfLuminance uses the calibration to convert luminance to color
 % index.
 % See also LuminanceOfIndex.
-
+plusMinusChar=char(177); % Use this instead of literal plus minus sign to prevent corruption of this non-ASCII character.
 image=cal.nFirst+(cal.nLast-cal.nFirst)*(imageLuminance-cal.LFirst)/(cal.LLast-cal.LFirst);
 image=round(image);
-ii=round(image(:))<cal.nFirst | round(image(:))>cal.nLast;
+ii=image(:)<cal.nFirst | image(:)>cal.nLast;
 if any(ii)
-    msg0=sprintf('Contrast too high? Luminances %dx%dx%d (mean %.1f) ranging %.1f to %.1f exceed linearized range %.1f to %.1f cd/m^2. ',...
-       size(imageLuminance,1),size(imageLuminance,2),size(imageLuminance,3),mean(imageLuminance(:)),min(imageLuminance(:)),max(imageLuminance(:)),cal.LFirst,cal.LLast);
+    msg0=sprintf('Contrast too high? Luminances %dx%dx%d (mean%csd %.1f%c%.1f), sd/mean %.1f, ranging %.1f to %.1f exceed linearized range %.1f to %.1f cd/m^2. ',...
+       size(imageLuminance,1),size(imageLuminance,2),size(imageLuminance,3),...
+       plusMinusChar,mean(imageLuminance(:)),plusMinusChar,std(imageLuminance(:)),...
+       std(imageLuminance(:))/mean(imageLuminance(:)),...
+       min(imageLuminance(:)),max(imageLuminance(:)),cal.LFirst,cal.LLast);
     msg1=sprintf('%.0f out-of-range pixels, with values [',sum(ii));
     msg2=sprintf(' %.0f',unique(image(ii)));
     msg3=sprintf('], were bounded to the range %d to %d.',cal.nFirst,cal.nLast);
