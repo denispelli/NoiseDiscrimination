f7b929c79239035329fa8a0e657e2be91a3bfdda
denispelli <denis.pelli@nyu.edu>
2019-05-14 02:56:31 -0400

    fixed bug
    renamed desiredTargetHeightPix to desiredTargetHeightChecks
    renamed sRect to sRectChecks
    changed
    ratio=desiredTargetHeightPix/o.targetHeightPix;
    to
    ratio=desiredTargetHeightChecks/RectHeight(sRectChecks);
    I believe the ratio was formerly low by a factor of 1/targetCheckPix.
    Indeed the biggest face I could get when measuring a size threshold was about 1/3 the stimulusRect height.

--- lib/ResizeImage.m
+++ lib/ResizeImage.m
@@ -1,27 +1,27 @@
-function oNew=ResizeImage(o,i,desiredTargetHeightPix)
-% o=ResizeImage(oo(oi),desiredTargetHeightPix);
-% Scale just o.signal(i).image to size specified by desiredTargetHeightPix.
-% For speed, we rescale only that image, selected by the index i.
+function oNew=ResizeImage(o,si,desiredTargetHeightChecks)
+% o=ResizeImage(oo(oi),desiredTargetHeightChecks);
+% Scale o.signal(si).image to size specified by desiredTargetHeightChecks.
+% For speed, we rescale only that image, selected by the index si.
 % denis.pelli@nyu.edu, March, 2019
-if desiredTargetHeightPix<1
-    error('desiredTargetHeightPix %.0f too small.',desiredTargetHeightPix);
+if desiredTargetHeightChecks<1
+    error('desiredTargetHeightChecks %.1f too small.',desiredTargetHeightChecks);
 end
-sRect=RectOfMatrix(o.signal(i).image); % units of targetChecks
-ratio=desiredTargetHeightPix/o.targetHeightPix;
-targetRectLocal=ceil(ratio*sRect);
-if RectWidth(targetRectLocal)~=RectWidth(o.targetRectLocal)
+sRectChecks=RectOfMatrix(o.signal(si).image); % units of targetChecks
+ratio=desiredTargetHeightChecks/RectHeight(sRectChecks);
+targetRectChecks=round(ratio*sRectChecks);
+if RectWidth(targetRectChecks)~=RectWidth(o.targetRectChecks)
     % We use the 'bilinear' method to make sure that all new pixel values
     % are within the old range. That's important because we set up the CLUT
     % with that range. Any pixels outside that range could have arbitrary
     % color and brightness.
     %% Scale to desired size.
-    sz=[RectHeight(targetRectLocal) RectWidth(targetRectLocal)];
-    o.signal(i).image=imresize(o.signal(i).image,sz,'bilinear');
-    o.signal(i).bounds=ImageBounds(o.signal(i).image,1);
-    sRect=RectOfMatrix(o.signal(i).image); % units of targetChecks
+    sz=[RectHeight(targetRectChecks) RectWidth(targetRectChecks)];
+    o.signal(si).image=imresize(o.signal(si).image,sz,'bilinear');
+    o.signal(si).bounds=ImageBounds(o.signal(si).image,1);
+    sRectChecks=RectOfMatrix(o.signal(si).image); % units of targetChecks
 end
-o.targetRectLocal=sRect;
-o.targetHeightOverWidth=RectHeight(sRect)/RectWidth(sRect);
-o.targetHeightPix=RectHeight(sRect)*o.targetCheckPix;
+o.targetRectChecks=sRectChecks;
+o.targetHeightOverWidth=RectHeight(sRectChecks)/RectWidth(sRectChecks);
+o.targetHeightPix=RectHeight(sRectChecks)*o.targetCheckPix;
 oNew=o;
 end
