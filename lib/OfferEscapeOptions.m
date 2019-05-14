281138f180d9bc7307a4bde3f58f626657fe8d7c
denispelli <denis.pelli@nyu.edu>
2019-05-14 06:06:50 -0400

    cosmetic, delete unused code

--- lib/OfferEscapeOptions.m
+++ lib/OfferEscapeOptions.m
@@ -25,17 +25,6 @@ if isfield(o,'trials') && isfield(o,'trialsPerBlock')
         trialsDesired=trialsDesired+oo(oi).trialsPerBlock;
     end
 end
-% if isfield(o,'block') && isfield(o,'blocksDesired') && isfield(o,'textSize')
-%    message=sprintf('Trial %d of %d. Block %d of %d.',trials,trialsDesired,o.block,o.blocksDesired);
-%    if isfield(o,'experiment')
-%       message=[message ' Experiment "' o.experiment '".'];
-%    end
-%    Screen('DrawText',window,message,o.textSize/2,o.textSize/2,black,backgroundColor);
-%    y=o.textSize;
-% else
-%     y=0;
-% end
-y=0;
 DrawCounter(o);
 Screen('TextSize',window,o.textSize);
 % Set background color for DrawFormattedText.
@@ -55,7 +44,7 @@ string=['You escaped. Any incomplete trial was canceled. ' ...
     'Hit RETURN to proceed to next block. ' ...
     'Hit ESCAPE again to quit the whole experiment. '...
     nextBlockMsg nextTrialMsg];
-DrawFormattedText(window,string,textMarginPix,textMarginPix+0.5*o.textSize+y,black,60,[],[],1.1);
+DrawFormattedText(window,string,textMarginPix,textMarginPix+0.5*o.textSize,black,60,[],[],1.1);
 Screen('Flip',window);
 answer=GetKeypress([spaceKeyCode returnKeyCode escapeKeyCode graveAccentKeyCode],o.deviceIndex);
 quitExperiment=ismember(answer,[escapeChar,graveAccentChar]);
