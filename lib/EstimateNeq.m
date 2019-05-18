c608af0efcd4001d13e3f819a223f6938b5f9170
denispelli <denis.pelli@nyu.edu>
2019-05-18 01:04:03 -0400

    fixed to cope with ideal observer data

--- lib/EstimateNeq.m
+++ lib/EstimateNeq.m
@@ -1,14 +1,22 @@
-function [E0,Neq]=EstimateNeq(E,N,ok)
-% We estimate the best linear fit of E vs N, taking into account the
-% conservation of sd of log E. The minimization begins with a quick guess
-% that is a linear regression that doesn't take that into account (i.e.
-% assuming conservation of sd of E, not log E). We use the regression
-% result as a starting point for the minimization. We constrain the
-% solution to nonnegative values of E0 and Neq. We use the optional logical
-% array "ok", to ignore bad thresholds.
+function [E0,Neq,deltaEOverN]=EstimateNeq(E,N,ok)
+% [E0,Neq,deltaEOverN]=EstimateNeq(E,N,ok);
+% We estimate the best linear fit of E vs N, 
+%    E = k*(N+Neq)
+% where E0=k*Neq is E in zero noise. Thus k=E0/Neq and
+%    E = (E0/Neq)*(N+Neq)
+% This fails in the special case of the ideal observer, for whom E0 and Neq
+% are both zero. So we also return deltaEOverN=(E-E0)/N, representing the
+% fit
+%    E = deltaEOverN*(N+Neq)
+% Our fit takes into account the conservation of sd of log E. The
+% minimization begins with a quick guess that is a linear regression that
+% doesn't take that into account (i.e. assuming conservation of sd of E,
+% not log E). We use the regression result as a starting point for the
+% minimization. We constrain the solution to nonnegative values of E0 and
+% Neq. We use the optional logical array "ok", to ignore bad thresholds.
 %
 % It was making terrible fits. mincon with the default algorithm often gave
-% wrong answers. Using the option to select the ?sqp? algorithm helped a
+% wrong answers. Using the option to select the 'sqp' algorithm helped a
 % lot. We now try an unconstrained fit first, which is more reliable, and
 % only if the answer is out of bounds do we try the constrained fit.
 %
@@ -19,19 +27,25 @@ function [E0,Neq]=EstimateNeq(E,N,ok)
 % small Neq. That's why we take the absolute value of Neq in choosing a
 % seed for the next fit.
 %
+% May 2019. This function was formerly parameterized by E0 and Neq. It
+% could not fit the ideal, for which both are zero. I reparameterized using
+% E0 and deltaEOverN. I hope this will now handle all cases.
+%
 % May 2018. denis.pelli@nyu.edu
 printFit=false;
 if nargin<3
-    ok=true(size(E));
+    ok=isfinite(E); % Ignore NaN and inf data.
 end
 switch sum(ok(:))
     case 0
         E0=nan;
         Neq=nan;
+        deltaEOverN=nan;
         return
     case 1
         E0=E(ok);
         Neq=nan;
+        deltaEOverN=E(ok)/N(ok);
         return
 end
 assert(all(size(N)==size(E)),'E and N must have same size.');
@@ -56,26 +70,28 @@ w=warning('OFF','MATLAB:singularMatrix');
 b=[ones(size(N)) N]\E; % Regress.
 warning(w);
 E0=b(1); % Guess E0
-Neq=E0/b(2); % Guess Neq
+deltaEOverN=b(2); % Guess deltaEOverN
+Neq=E0/deltaEOverN; % Guess Neq
 if printFit
     fprintf('Initial regression, Neq %.2g, E0 %.2g, rms error of log E %.2g\n',Neq,E0,Cost(E,N,E0,Neq));
 end
 E0=max(E0,eps); % Impose positivity on the guess, so mincon won't fail.
-Neq=abs(Neq); % Impose positivity on the guess, so mincon won't fail.
+deltaEOverN=abs(deltaEOverN); % Impose positivity on the guess, so mincon won't fail.
+% Neq=abs(Neq); % Impose positivity on the guess, so mincon won't fail.
 fun=@(b) Cost(E,N,b(1),b(2));
 opts=optimset('TypicalX',[0.1 1e-6],'MaxFunEvals',1e6);
-b=fminsearch(fun,[E0 Neq],opts); % Unconstrained fit.
+b=fminsearch(fun,[E0 deltaEOverN],opts); % Unconstrained fit.
 E0=b(1);
-Neq=b(2);
+deltaEOverN=b(2);
 if printFit
-    fprintf('fminsearch fit, Neq %.2g, E0 %.2g, rms error of log E %.2g\n',Neq,E0,Cost(E,N,E0,Neq));
+    fprintf('fminsearch fit, deltaEOverN %.2g, E0 %.2g, rms error of log E %.2g\n',deltaEOverN,E0,Cost(E,N,E0,Neq));
 end
 % The unconstrained fit is more reliable, so we use its answer when it's in
 % bounds. If it's out of bounds, then we run a constrained fit.
-if E0<0 || Neq<0
-    unconstrainedCost=Cost(E,N,E0,Neq);
+if E0<0 || deltaEOverN<0
+    unconstrainedCost=Cost(E,N,E0,deltaEOverN);
     E0=max(eps,E0);
-    Neq=abs(Neq); 
+    deltaEOverN=abs(deltaEOverN); 
     % The choice of algorithm is key. With the default algorithm I was
     % getting terrible fits. 'sqp' works well.
     opts=optimoptions('fmincon','Display','off','Algorithm','sqp','TypicalX',[0.1 1e-6]);
@@ -83,20 +99,21 @@ if E0<0 || Neq<0
     b=fmincon(fun,[E0 Neq],[],[],[],[],[eps eps],[inf inf],[],opts); % Search constrains E0 and Neq to not be negative.
     warning(w);
     E0=b(1);
-    Neq=b(2);
+    deltaEOverN=b(2);
     if printFit
-        fprintf('mincon fit, Neq %.2g, E0 %.2g, rms error of log E %.2g\n',Neq,E0,Cost(E,N,E0,Neq));
+        fprintf('mincon fit, deltaEOverN %.2g, E0 %.2g, rms error of log E %.2g\n',deltaEOverN,E0,Cost(E,N,E0,deltaEOverN));
     end
 end
-cost=Cost(E,N,E0,Neq);
+cost=Cost(E,N,E0,deltaEOverN);
 if cost>0.5
-    warning('The rms error in fitting log E is %.2f, which is terribly large. E0 %.2g, Neq %.2g',cost,E0,Neq);
+    warning('The rms error in fitting log E is %.2f, which is terribly large. E0 %.2g, deltaEOverN %.2g',cost,E0,deltaEOverN);
 end
+deltaEOverN=b(2);
 end
 
-function cost=Cost(E,N,E0,Neq)
+function cost=Cost(E,N,E0,deltaEOverN)
 % Compute RMS error in predicting log E. We use log because the sd of
 % measurement is approximately conserved in log E, and not in E.
-modelE=(E0/Neq)*(N+Neq);
+modelE=E0+deltaEOverN*N;
 cost=sqrt(mean(log10(E./modelE).^2));
 end
