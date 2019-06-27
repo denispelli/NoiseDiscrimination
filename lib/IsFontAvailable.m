f1dbe73ae3cc59e85e95fef2e8ed1231870af103
denispelli <denis.pelli@nyu.edu>
2019-06-27 00:20:21 -0400

    optional warning if font missing

--- lib/IsFontAvailable.m
+++ lib/IsFontAvailable.m
@@ -1,11 +1,17 @@
-function ok=IsFontAvailable(font)
-% ok=IsFontAvailable(font)
-% "font" is a string or a cell array of strings. Each string is a font name.
-% Returns a logical array, one element per font, indicating true if
-% available.
-if isempty(font)
+function ok=IsFontAvailable(font,warn)
+% ok=IsFontAvailable(font,warn)
+% "font" is a string or a cell array of strings. Each string is a font
+% name. Returns a logical array, one element per font, indicating true if
+% the font is available. If the optional argument "warn" is the string
+% 'warn' then a warning is printed for each missing font.
+% Denis Pelli, June 26, 2019
+% denis.pelli@nyu.edu
+if nargin<1 || isempty(font)
     ok=logical([]);
     return
+end
+if nargin<2
+    warn='';
 end
 switch class(font)
     case 'cell'
@@ -14,54 +20,48 @@ switch class(font)
         fonts={font};
 end
 myPath=fileparts(mfilename('fullpath')); % Takes 0.1 s.
-o.useFractionOfScreenToDebug=0.25;
+o.useFractionOfScreenToDebug=0.1;
 o.screen=0;
-% fprintf('Opening the window. ...\n'); % Newline for Screen warnings.
-s=GetSecs;
 screenBufferRect=Screen('Rect',o.screen);
 r=round(o.useFractionOfScreenToDebug*screenBufferRect);
 r=AlignRect(r,screenBufferRect,'right','bottom');
 windows=Screen('Windows');
 oldVerbosity=Screen('Preference','Verbosity',0);
 if isempty(windows)
-    % Nothings open, so open a window.
-    [window,o.screenRect]=Screen('OpenWindow',o.screen,1.0,r);
+    % Nothing's open, so open a window.
+    window=Screen('OpenWindow',o.screen,1.0,r);
     if Screen(window,'WindowKind')~=1
-        error('Failed attempt to open window.');
+        error('Failed attempt to open a window.');
     end
 else
     % There already is an open window, so open an offscreen window.
     window=[];
     for i=1:length(windows)
         if Screen(windows(i),'WindowKind')==1
-            [window,o.screenRect]=Screen('OpenOffscreenWindow',o.screen,1.0,r);
+            window=Screen('OpenOffscreenWindow',o.screen,1.0,r);
             break;
         end
     end
     if Screen(window,'WindowKind')~=-1
-        error('Failed attempt to open offscreen window.');
+        error('Failed attempt to open an offscreen window.');
     end
 end
-% fprintf('Done opening window (%.1f s).\n',GetSecs-s);
 
-%% CHECK FOR NEEDED FONTS
-test=struct([]);
-ok=logical([]);
+%% CHECK FOR SPECIFIED FONTS
 for i=1:length(fonts)
     font=fonts{1};
-    test(end+1).name=sprintf('%s font',font);
-    Screen('TextFont',window,font);
-    % Perform dummy DrawText call, in case the OS has deferred settings.
+    oldFont=Screen('TextFont',window,font);
+    % Perform dummy DrawText call, in case the OS defers setting of the font.
     Screen('DrawText',window,' ',0,0);
-    oldFont=Screen('TextFont',window);
-    test(end).value=streq(oldFont,font);
-    ok(i)=test(end).value;
-    if ~test(end).value
-        warning('The font "%s" is not available. Please install it.',font);
+    newFont=Screen('TextFont',window);
+    Screen('TextFont',window,oldFont); % Restore old font.
+    ok(i)=streq(newFont,font);
+    if ~ok(i) && ismember(warn,{'warn'})
+        s=warning('QUERY','BACKTRACE');
+        warning OFF BACKTRACE
+        warning('The font ''%s'' is not available. Please install it.',font);
+        warning(s);
     end
-    test(end).min=true;
-    test(end).ok=test(end).value;
-    test(end).help=['dir ' fullfile(myPath,'fonts')];
 end
 Screen('Close',window);
 Screen('Preference','Verbosity',oldVerbosity);
