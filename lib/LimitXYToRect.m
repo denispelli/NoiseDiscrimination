7f9caa22157f7d50dd25769332a1d93633d31b50
denispelli <denis.pelli@nyu.edu>
2019-10-17 13:52:02 -0400

    polish

--- lib/LimitXYToRect.m
+++ lib/LimitXYToRect.m
@@ -1,15 +1,20 @@
 function xy=LimitXYToRect(xy,rect)
 % xy=LimitXYToRect(xy,rect);
 % Restrict x and y to lie inside rect.
-% If xy is outside rect, return nearest point on rect.
 % If xy is inside rect, return it.
-% denis.pelli@nyu.edu, 2018
-if ~all(rect(1:2)<=rect(3:4))
-    error(['Illegal rect [%.0f %.0f %.0f %.0f]. ' ...
-        'Upper left corner should not be below or ' ...
-        'right of lower right corner.'],...
-        rect(1),rect(2),rect(3),rect(4));
+% If xy is outside rect, return nearest point on rect.
+% Rect is defined by two points, [x1 y1 x2 y2].
+% In Apple screen coordinates y increases downward, 
+% whereas in ordinary coordinates, y increases upward.
+% Thus we assume x2 >= x1, but we don't assume any 
+% relation between y2 and y1. 
+% denis.pelli@nyu.edu, 2019
+
+if rect(2)>rect(4)
+     rect=rect([1 4 3 2]);
 end
+% Now we can assume that x and y are both increasing within the new rect,
+% and use that rect to limit xy.
 xy=max(xy,rect(1:2));
 xy=min(xy,rect(3:4));
 end
