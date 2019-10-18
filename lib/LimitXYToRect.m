a02d7a57f062c12ddfd2dd3c776a1893e683b0e7
denispelli <denis.pelli@nyu.edu>
2019-10-18 01:47:27 -0400

    polish

--- lib/LimitXYToRect.m
+++ lib/LimitXYToRect.m
@@ -1,13 +1,10 @@
 function xy=LimitXYToRect(xy,rect)
 % xy=LimitXYToRect(xy,rect);
-% Restrict x and y to lie inside rect.
-% If xy is inside rect, return it.
-% If xy is outside rect, return nearest point on rect.
-% Rect is defined by two points, [x1 y1 x2 y2].
-% In Apple screen coordinates y increases downward, 
-% whereas in ordinary coordinates, y increases upward.
-% Thus we assume x2 >= x1, but we don't assume any 
-% relation between y2 and y1. 
+% Restrict x and y to lie inside rect. If xy is inside rect, return it. If
+% xy is outside rect, return nearest point on rect. rect is defined by two
+% points, [x1 y1 x2 y2]. In Apple screen coordinates y increases downward,
+% whereas in ordinary coordinates, y increases upward. Thus we assume 
+% x2 >= x1, but we don't assume any relation between y2 and y1.
 % denis.pelli@nyu.edu, 2019
 
 if rect(2)>rect(4)
