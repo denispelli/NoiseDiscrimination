5de42b6ded50a94498e60f0543368bd6a7789e5c
denispelli <denis.pelli@nyu.edu>
2020-02-20 00:49:00 -0500

    enhanced to also return all SDs

--- lib/MaxNoiseSD.m
+++ lib/MaxNoiseSD.m
@@ -1,20 +1,27 @@
 function [maxNoiseSD,allSD]=MaxNoiseSD(noiseType,maxBound)
 % [maxNoiseSD,allSD]=MaxNoiseSD(noiseType [,maxBound]);
-% This returns the value of SD for the given kind of noise that will just
+% Returns the value of SD, for the given kind of noise, that will just
 % barely respect the bounds [-maxBound maxBound]. With the same bound on
 % range, we can reach 3.3 times higher noiseSD using binary instead of
 % gaussian noise.
-% 'gaussian' noise has PDF given by a zero-mean true Gaussian clipped at
-% +/-2 SD of true Gaussian. The returned SD is the SD of the clipped
-% distribution. 'uniform' noise has equal probability density over the
-% range [-maxBound maxBound]. 'binary' has 0.5 probabity at each of
+%
+% "noiseType": 'gaussian' noise has PDF given by a zero-mean true Gaussian
+% clipped at +/-2 SD of true Gaussian. The returned SD is the SD of the
+% clipped distribution. 'uniform' noise has equal probability density over
+% the range [-maxBound maxBound]. 'binary' has 0.5 probabity at each of
 % [-maxBound maxBound]. 'ternary' has 1/3 probability at each of [-maxBound
 % 0 maxBound].
+%
+% "maxBound": The default values of maxBound, 0.37, is based on lots of
+% experience and has worked well.
+%
 % SUMMARY:
 % gaussian: 0.1591
 %  uniform: 0.2146
 %  ternary: 0.3700
 %   binary: 0.5233
+%
+% denis.pelli@nyu.edu, February 2020
 if nargin<2
     maxBound=0.37; % Rule of thumb based on experience with gaussian.
 end
