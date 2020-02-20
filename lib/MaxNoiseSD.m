cb7500c000add4118c753060ec5849a6896e652d
denispelli <denis.pelli@nyu.edu>
2020-02-19 20:59:32 -0500

    minor enhancement to optionally return all values as a struct

--- lib/MaxNoiseSD.m
+++ lib/MaxNoiseSD.m
@@ -1,29 +1,37 @@
-function maxNoiseSD=MaxNoiseSD(noiseType)
-% maxNoiseSD=MaxNoiseSD(noiseType);
+function [maxNoiseSD,allSD]=MaxNoiseSD(noiseType,maxBound)
+% [maxNoiseSD,allSD]=MaxNoiseSD(noiseType [,maxBound]);
 % This returns the value of SD for the given kind of noise that will just
 % barely respect the bounds [-maxBound maxBound]. With the same bound on
 % range, we can reach 3.3 times higher noiseSD using binary instead of
 % gaussian noise.
-% "Gaussian" noise has PDF given by a zero-mean true Gaussian clipped at
+% 'gaussian' noise has PDF given by a zero-mean true Gaussian clipped at
 % +/-2 SD of true Gaussian. The returned SD is the SD of the clipped
-% distribution. Uniform has equal probability density over the range
-% [-maxBound maxBound]. Binary has 0.5 probabity at each of [-maxBound
-% maxBound]. Ternary has 1/3 probability at each of [-maxBound 0 maxBound].
+% distribution. 'uniform' noise has equal probability density over the
+% range [-maxBound maxBound]. 'binary' has 0.5 probabity at each of
+% [-maxBound maxBound]. 'ternary' has 1/3 probability at each of [-maxBound
+% 0 maxBound].
+% SUMMARY:
+% gaussian: 0.1591
+%  uniform: 0.2146
+%  ternary: 0.3700
+%   binary: 0.5233
+if nargin<2
+    maxBound=0.37; % Rule of thumb based on experience with gaussian.
+end
 persistent maxSD
 if isempty(maxSD)
     % sDOverBound is ratio of distribution's SD over its +/- bound.
     sDOverBound.gaussian=0.43;
     sDOverBound.uniform=0.58;
-    sDOverBound.binary=std([-1 1]);
     sDOverBound.ternary=std(-1:1);
-    maxBound=0.37; % Rule of thumb based on experience with gaussian.
+    sDOverBound.binary=std([-1 1]);
     maxSD=struct('gaussian',maxBound*sDOverBound.gaussian,...
         'uniform',maxBound*sDOverBound.uniform,...
-        'binary',maxBound*sDOverBound.binary,...
-        'ternary',maxBound*sDOverBound.ternary);
+        'ternary',maxBound*sDOverBound.ternary,...
+        'binary',maxBound*sDOverBound.binary);
 end
-if ~ismember(noiseType,{'gaussian' 'binary' 'ternary' 'uniform'})
+if ~ismember(noiseType,{'gaussian' 'uniform' 'ternary' 'binary'})
     error('Unknown noiseType ''%s''.',noiseType);
 end
 maxNoiseSD=maxSD.(noiseType);
-
+allSD=maxSD;
