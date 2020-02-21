2a6cb13ba0028f95842c6ecc10d466df25f220c6
denispelli <denis.pelli@nyu.edu>
2020-02-21 03:42:04 -0500

    enhanced to handle observer and ideal

--- analyzeNoiseTest.m
+++ analyzeNoiseTest.m
@@ -48,20 +48,15 @@ if isfield(oo,'partingComments')
     end
 end
 
-% COMPUTE EFFICIENCY
-% Select thresholdParameter='contrast', for each conditionName,
-% For each observer, including ideal, use all (E,N) data to estimate deltaNOverE and Neq.
-% Compute efficiency by comparing deltaNOverE of each to that of the ideal.
-conditionNames=unique({oo.conditionName});
-observers=unique({oo.observer});
-
 % Round each target size to a power of 2.
 for oi=1:numel(oo)
     oo(oi).targetHeightDeg = 2.^round(log2([oo(oi).targetHeightDeg]));
 end
 
-% We average all thresholds that match in:
+% Average all thresholds that match in:
 % noiseType, conditionName, observer, targetHeight, and eccentricityXY.
+conditionNames=unique({oo.conditionName});
+observers=unique({oo.observer});
 targetHeightDegs=unique([oo.targetHeightDeg]);
 eccXsfull=arrayfun(@(x) x.eccentricityXYDeg(1),oo);
 eccXs=unique(eccXsfull);
@@ -113,15 +108,11 @@ for conditionName=conditionNames
                         & ismember([aa.targetHeightDeg],targetHeightDeg)...
                         & ismember([aa.eccentricityDeg], eccX) ...
                         & ismember({aa.noiseType},noiseType);
-                    if targetHeightDeg>32
-                        keyboard
-                    end
                     idealMatch=match & ismember({aa.observer},{'ideal'});
                     match = match & ismember({aa.observer},observer);
                     if sum(match)>0 && sum(idealMatch)>0
                         assert(sum(match)==1 & sum(idealMatch)==1);
-                        aa(match).efficiency=idealEOverN.(conditionName{1})/aa(match).deltaEOverN;
-                        % aa(match).efficiency=aa(idealMatch).deltaEOverN/aa(match).deltaEOverN;
+                        aa(match).efficiency=(aa(idealMatch).E/aa(idealMatch).N)/aa(match).deltaEOverN;
                     end
                 end
             end
@@ -129,88 +120,100 @@ for conditionName=conditionNames
     end
 end
 for i=1:length(aa)
+    % Convert noiseType (a name) to noiseIndex (an integer).
     aa(i).noiseIndex=find(ismember(noiseTypes,aa(i).noiseType));
 end
 % human=~ismember({aa.observer},'ideal');
 
+% Normalize the measured threshold contrast by the actual (not reported)
+% noiseSD.
+% switch noiseType % Fill noiseList with desired kind of noise.
+%     case 'gaussian'
+%         temp=randn([1 20000]);
+%         ok=-2<=temp & temp<=2;
+%         noiseList=temp(ok);
+%         clear temp;
+%     case 'uniform'
+%         noiseList=-1:1/1024:1;
+%     case 'binary'
+%         noiseList=[-1 1];
+%     case 'ternary'
+%         noiseList=[-1 0 1];
+%     otherwise
+%         error('%d: Unknown noiseType "%s"',oi,oo(oi).noiseType);
+% end
+% oldSD=std(noiseList);
+% noiseListSD=std(PsychRandSample(noiseList,[1000000 1]));
+% for iNoise=1:length(noiseTypes)
+%     stdReported
+
 t=struct2table(aa);
 t=sortrows(t,'conditionName');
+t=sortrows(t,'observer');
 disp(t(:,{'conditionName' 'targetHeightDeg' 'contrast' 'observer' 'noiseType' 'noiseIndex' 'noiseSD'}));
 dataFolder=fullfile(fileparts(mfilename('fullpath')),'data');
-% for iefficiency=1:length(t.efficiency)
-%   if isempty(t.efficiency{iefficiency})
-%     t.efficiency{iefficiency}=NaN;
-%   end
-% end
 writetable(t,fullfile(dataFolder,[experiment '.xls']));
 jsonwrite(fullfile(dataFolder,[experiment '.json']), t);
 
 a=table2struct(t);
-ii=1:4;
 % figure(1);
 figureHandle=figure('Name',experiment,'NumberTitle','off','pos',[10 10 500 800]);
 % orient 'landscape'; % For printing.
+degStyle={'r' 'g' 'b'};
+assert(length(degStyle)>=length(targetHeightDegs));
+observerStyle={'-*' '--o'};
+assert(length(observerStyle)>=length(observers));
 
-subplot(2,1,1);
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-r'); % 2
-hold on
-ii=ii+4;
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-g'); % 8
-ii=ii+4;
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-b'); % 32
-set(findall(gcf,'-property','FontSize'),'FontSize',12)
-title('Gabor','fontsize',18)
-xlabel('Noise type','fontsize',18);
-ylabel('Contrast threshold','fontsize',18);
-xlim([0 5]);
-ax = gca;
-ax.XTick = 1:4;
-ax.XTickLabels = {'Binary' 'Gaussian' 'Ternary' 'Uniform'};
-lgd=legend('Location','northwest','String',{'2 deg','8 deg','32 deg'},'fontsize',12);
-lgd.Box='off';
-title(lgd,'Size','fontsize',12)
-hold off
-
-% Scale log unit to be 18 cm, vertically.
-ax=gca;
-ax.YLim=[0.04 0.16];
-u=ax.Units;
-ax.Units='centimeters';
-drawnow; % Needed for valid Position reading.
-pos=ax.Position;
-ax.Position=[pos(1:3) 18*diff(log10(ax.YLim))];
-ax.Units=u;
-
-subplot(2,1,2);
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-r'); % 2
-hold on
-ii=ii+4;
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-g'); % 8
-ii=ii+4;
-semilogy([a(ii).noiseIndex],-[a(ii).contrast],'-b'); % 32
-title('Letter','fontsize',18)
-xlabel('Noise type','fontsize',18);
-ylabel('Contrast threshold','fontsize',18);
-xlim([0 5]);
-ax = gca;
-ax.XTick = 1:4;
-ax.XTickLabels = {'Binary' 'Gaussian' 'Ternary' 'Uniform'};
-lgd=legend('Location','northwest','String',{'2 deg','8 deg','32 deg'},'fontsize',12);
-lgd.Box='off';
-title(lgd,'Size','fontsize',12)
-hold off
-
-% Scale log unit to be 18 cm, vertically.
-ax=gca;
-ax.YLim=[0.04 0.16];
-u=ax.Units;
-ax.Units='centimeters';
-drawnow; % Needed for valid Position reading.
-pos=ax.Position;
-ax.Position=[pos(1:3) 18*diff(log10(ax.YLim))];
-ax.Units=u;
-
-set(findall(gcf,'-property','FontSize'),'FontSize',12);
+for conditionName=conditionNames
+    switch conditionName{1}
+        case 'letter'
+            subplot(2,1,1);
+        case 'gabor'
+            subplot(2,1,2);
+    end
+    for iObserver=1:length(observers)
+        for iDeg=1:length(targetHeightDegs)
+            for eccX=eccXs
+                match=ismember({a.thresholdParameter},{'contrast'})...
+                    & ismember({a.conditionName},conditionName)...
+                    & ismember([a.targetHeightDeg],targetHeightDegs(iDeg))...
+                    & ismember([a.eccentricityDeg],eccX);
+                %         idealMatch=match & ismember({a.observer},{'ideal'});
+                match = match & ismember({a.observer},observers(iObserver));
+                semilogy([a(match).noiseIndex],-[a(match).contrast],...
+                    [degStyle{iDeg} observerStyle{iObserver}],...
+                    'DisplayName',sprintf('%2.0f deg %s',...
+                    targetHeightDegs(iDeg),observers{iObserver}));
+%                 conditionName, {a(match).conditionName}, observers{iObserver}, {a(match).observer}
+%                 targetHeightDegs(iDeg), [a(match). targetHeightDeg]
+%                 -[a(match).contrast]
+                hold on
+            end
+        end
+    end
+    xlim([-1.5 4.5]);
+    ax=gca;
+    ax.XTick=1:4;
+    ax.XTickLabels={'Binary' 'Gaussian' 'Ternary' 'Uniform'};
+    lgd=legend('Location','northwest','Box','off');
+    title(lgd,'Size and Observer');
+    lgd.FontName='Monaco';
+    name=conditionName{1};
+    title([upper(name(1)) name(2:end)],'fontsize',18)
+    xlabel('Noise type','fontsize',18);
+    ylabel('Contrast threshold','fontsize',18);
+    set(findall(gcf,'-property','FontSize'),'FontSize',12)
+    % lgd.FontSize=10;
+    if true
+        % Scale log unit to be 12 cm vertically.
+        ax=gca;
+        %ax.YLim=[0.005 0.16];
+        ax.Units='centimeters';
+        drawnow; % Needed for valid Position reading.
+        ax.Position(4)=8*diff(log10(ax.YLim));
+    end
+    hold off
+end
 
 % Save plot to disk
 graphFile=fullfile(fileparts(mfilename('fullpath')),'data',[experiment '.eps']);
