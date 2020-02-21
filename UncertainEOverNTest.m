598d3a0c2bba4f87278284b4fbce9ed33bce7e49
denispelli <denis.pelli@nyu.edu>
2020-02-21 03:42:56 -0500

    minor

--- UncertainEOverNTest.m
+++ UncertainEOverNTest.m
@@ -1,6 +1,12 @@
+M=[1 10];
 psych.targetKind='gabor';
 psych.targetKind='letter';
 psych.noiseSD=0.17;
-M=[1 104];
+psych.noiseType='gaussian';
 [EOverN,psych]=UncertainEOverN(M,psych);
+psych.noiseType
+EOverN
+psych.noiseType='uniform';
+[EOverN,psych]=UncertainEOverN(M,psych);
+psych.noiseType
 EOverN
