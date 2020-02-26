b2ead6cfc798efe99d43c37ea9a8279a49ff2d54
denispelli <denis.pelli@nyu.edu>
2020-02-26 01:44:08 -0500

    default 0 0

--- lib/NominalAcuityDeg.m
+++ lib/NominalAcuityDeg.m
@@ -4,6 +4,9 @@ function nominalAcuityDeg=NominalAcuityDeg(eccentricityXYDeg)
 % You can pass one or more eccentricities. Each row is one eccentry.
 % The result is a column with one value per row.
 % See also: NominalCriticalSpacingDeg
+if nargin<1
+    eccentricityXYDeg=[0 0];
+end
 assert(size(eccentricityXYDeg,2)==2)
 ecc=sqrt(sum(eccentricityXYDeg.^2,2));
 nominalAcuityDeg=0.029*(ecc+2.72);
