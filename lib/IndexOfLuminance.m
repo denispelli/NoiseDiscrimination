c2f224fb89c7d8ea1d5ccd2ea12cb647b005ec66
denispelli <denis.pelli@nyu.edu>
2020-04-16 23:51:03 -0400

    minor update

--- lib/IndexOfLuminance.m
+++ lib/IndexOfLuminance.m
@@ -3,7 +3,7 @@ function image = IndexOfLuminance(cal,imageLuminance)
 % IndexOfLuminance uses the calibration to convert luminance to color
 % index.
 % See also LuminanceOfIndex.
-plusMinusChar=char(177); % Use this instead of literal plus minus sign to prevent corruption of this non-ASCII character.
+plusMinusChar=char(177); % Use this instead of literal plus-minus sign to prevent corruption of this non-ASCII character.
 image=cal.nFirst+(cal.nLast-cal.nFirst)*(imageLuminance-cal.LFirst)/(cal.LLast-cal.LFirst);
 image=round(image);
 ii=image(:)<cal.nFirst | image(:)>cal.nLast;
@@ -16,7 +16,7 @@ if any(ii)
     msg1=sprintf('%.0f out-of-range pixels, with values [',sum(ii));
     msg2=sprintf(' %.0f',unique(image(ii)));
     msg3=sprintf('], were bounded to the range %d to %d.',cal.nFirst,cal.nLast);
-    warning('%s%s%s',msg0,msg1,msg2,msg3);
+    warning('IndexOfLuminance: %s%s%s',msg0,msg1,msg2,msg3);
     image=min(image,cal.nLast);
     image=max(image,cal.nFirst);
 end
