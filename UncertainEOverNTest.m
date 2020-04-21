724e35c4390a67c813e0693eaf7f7666c6df5727
denispelli <denis.pelli@nyu.edu>
2020-04-20 23:37:44 -0400

    measure efficiency vs uncertainty and number of signals

--- UncertainEOverNTest.m
+++ UncertainEOverNTest.m
@@ -1,13 +1,19 @@
-M=[1 10];
-psych.targetKind='gabor';
-psych.targetKind='letter';
-psych.noiseSD=0.17;
-psych.reps=1000;
+clear all
+Screen('Preference','Verbosity',0);
+% psych.trialsDesired=100;
+psych.trialsDesired=300;
+% psych.reps=100;
+psych.reps=12;
+% psych.noiseSD=0.17;
+% psych.pThreshold=0.64;
 psych.noiseType='gaussian';
-[EOverN,psych]=UncertainEOverN(M,psych);
-psych.noiseType
-EOverN
-psych.noiseType='uniform';
-[EOverN,psych]=UncertainEOverN(M,psych);
-psych.noiseType
-EOverN
+psych.getAlphabetFromDisk=false;
+M=[1 100];
+for n=[2 9]
+    a='DKHNORSVZ';
+    psych.alphabet=a(1:n);
+    for targetKind={'gabor' 'orthogonalLetter' 'letter'}
+        psych.targetKind=targetKind{1};
+        [EOverN,psych]=UncertainEOverN(M,psych);
+    end
+end
